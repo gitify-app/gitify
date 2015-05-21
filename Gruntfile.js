812963998035534a573924e71a7c448e4696af46
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-05-21 19:49:47 +0100

    Gruntfile and README.md

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    less: {
+      options: {
+        cleancss: true
+      },
+      main: {
+        files: {
+          'build/css/style.css': 'src/less/style.less'
+        }
+      }
+    },
+
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: "src/fonts/", src: '**', dest: 'build/fonts/'},
+          {expand: true, cwd: "node_modules/bootstrap/fonts/", src: '**', dest: 'build/fonts/'},
+          {expand: true, cwd: "node_modules/font-awesome/fonts/", src: '**', dest: 'build/fonts/'}
+        ]
+      }
+    },
+
+    watch: {
+      less: {
+        files: 'src/less/*',
+        tasks: ['less'],
+      }
+    },
+
+    clean: {
+        files: ["build/"]
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Default task(s).
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['less', 'copy']);
+  grunt.registerTask('release', ['clean', 'build']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# github-notifier
-An app that sits next to your desktop's clock and notifies your for any GitHub notifications.
+# Github Notifier
+==========
+Github Notifications on your menu bar.
+
+### Prerequisites
+
+ - Electron
+ - ReactJS
+ - Grunt
+ - NPM
+
+
+### Installation
+If you encounter any issues with `npm install`, then run `ulimit -n 512`.
+
+    npm install
+
+
+### Development
+To watch for changes in the `src` directory:
+
+    npm run watch
+
+To run the actual **electron app**:
+
+    npm start
+
+
+### Tests (JsxHint)
+
+    npm test
