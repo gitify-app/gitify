3dc7d1c158ace7a38a7ffb6426818f28a3d5e631
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-05-23 00:19:18 +0100

    Set opacity and prevent 'reading' it again

--- src/js/components/notification.js
+++ src/js/components/notification.js
@@ -8,18 +8,30 @@ var apiRequests = require('../utils/api-requests');
 
 var Notification = React.createClass({
 
+  getInitialState: function() {
+    return {
+      readClass: 'row notification',
+      read: false
+    };
+  },
+
   openBrowser: function () {
     var url = this.props.notification.subject.url.replace('api.github.com/repos', 'www.github.com');
     shell.openExternal(url);
   },
 
   markAsRead: function () {
+    var self = this;
+    if (this.state.read) { return; }
     apiRequests
       .patchAuth('https://api.github.com/notifications/threads/' + this.props.notification.id)
       .end(function (err, response) {
         if (response && response.ok) {
           // Notification Read
-          // Remove from list?
+          self.setState({
+            readClass: self.state.readClass + ' read',
+            read: true
+          });
         } else {
           // Error - Show messages.
           console.log(err);
@@ -39,12 +51,12 @@ var Notification = React.createClass({
     }
 
     return (
-      <div className='row notification'>
+      <div className={this.state.readClass}>
         <div className='col-xs-1'><span className={typeIconClass} /></div>
         <div className='col-xs-10 subject' onClick={this.openBrowser}>
           {this.props.notification.subject.title}
         </div>
-        <div className='col-xs-1 check-wrapper'><span className="octicon octicon-check" onClick={this.markAsRead} /></div>
+        <div className='col-xs-1 check-wrapper'><span className='octicon octicon-check' onClick={this.markAsRead} /></div>
       </div>
     );
   }
