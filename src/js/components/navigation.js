76974ce91f49a52c6b9c00523e91485ccd4b3d84
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-05-25 15:51:02 +0100

    Rebranding

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -69,7 +69,7 @@ var Navigation = React.createClass({
       <div className='container-fluid'>
         <div className='row navigation'>
           <div className='col-xs-4 left'>{refreshIcon}</div>
-          <div className='col-xs-4 logo'>GitHub</div>
+          <div className='col-xs-4 logo'>Gitify</div>
           <div className='col-xs-4 right'>
             {logoutIcon}
             <i className="fa fa-power-off" onClick={this.appQuit} />
