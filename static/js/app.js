8f978b16f22a8199360621f0d4077d4c8e6a0361
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-09-11 22:54:05 +0100

    jQuery UI effectss

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,23 @@
 $(document).ready(function () {
+  $("#latest-version").hide();
+  $("#screenshot").hide().delay(500).slideToggle(1000,"easeOutBounce");
+
   getLatestReleaseDetails();
 });
 
 function getLatestReleaseDetails() {
   $.getJSON("https://api.github.com/repos/ekonstantinidis/gitify/releases/latest").done(function (release) {
-      var asset = release.assets[0];
-      var downloadURL = "https://github.com/ekonstantinidis/gitify/releases/download/" + release.tag_name + "/" + asset.name;
+    var asset = release.assets[0];
+    var downloadURL = "https://github.com/ekonstantinidis/gitify/releases/download/" + release.tag_name + "/" + asset.name;
 
-      var releaseDate = new Date(release.created_at);
-      var d = releaseDate.getDate();
-      var m =  releaseDate.getMonth() + 1;
-      var y = releaseDate.getFullYear();
+    var releaseDate = new Date(release.created_at);
+    var d = releaseDate.getDate();
+    var m =  releaseDate.getMonth() + 1;
+    var y = releaseDate.getFullYear();
 
-      $("#download-button").attr("href", downloadURL);
-      $("#latest-version .version span").text(release.tag_name);
-      $("#latest-version .date span").text(d + "/" + m + "/" + y);
+    $("#download-button").attr("href", downloadURL);
+    $("#latest-version .version span").text(release.tag_name);
+    $("#latest-version .date span").text(d + "/" + m + "/" + y);
+    $("#latest-version").fadeIn(500);
   });
 }
