5d1dc6b39014d2d1567583e40efecd38665d2fdc
Emmanouil Konstantinidis <manos@dabapps.com>
2015-09-17 14:13:11 +0100

    Handle both callbacks

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -35,11 +35,18 @@ var Login = React.createClass({
     var authUrl = githubUrl + 'client_id=' + options.client_id + '&scope=' + options.scope;
     authWindow.loadUrl(authUrl);
 
-    authWindow.webContents.on('did-get-redirect-request', function (event, oldUrl, newUrl) {
+    authWindow.webContents.on('will-navigate', function (event, url) {
+      handleCallback(url);
+    });
 
-      var raw_code = /code=([^&]*)/.exec(newUrl) || null;
+    authWindow.webContents.on('did-get-redirect-request', function (event, oldUrl, newUrl) {
+      handleCallback(newUrl);
+    });
+
+    function handleCallback (url) {
+      var raw_code = /code=([^&]*)/.exec(url) || null;
       var code = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
-      var error = /\?error=(.+)$/.exec(newUrl);
+      var error = /\?error=(.+)$/.exec(url);
 
       if (code || error) {
         // Close the browser if code found or error
@@ -53,8 +60,7 @@ var Login = React.createClass({
         alert('Oops! Something went wrong and we couldn\'t' +
           'log you in using Github. Please try again.');
       }
-
-    });
+    }
 
     // If "Done" button is pressed, hide "Loading"
     authWindow.on('close', function () {
