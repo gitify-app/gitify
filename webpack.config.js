ae71ada0d634f47529ad1f3ff92df59e63960d01
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-04-22 22:45:45 +0100

    Prepare redux & webpack hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
 var webpack = require('webpack');
 
 module.exports = {
-  context: __dirname + '/src/js',
-  entry: './app.js',
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
+
+  entry: {
+    dev: 'webpack-dev-server/client?http://0.0.0.0:3000',
+    app: './src/js/app.js'
+  },
+
   output: {
     path: __dirname + '/build/js',
-    filename: 'app.js'
+    filename: '[name].js',
+    publicPath: 'http://localhost:3000/build/js/'
   },
-  devtool: 'source-map',
+
   plugins: [
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      __DEV__: true,
+      'process.env': {
+        NODE_ENV: JSON.stringify('development')
+      }
+    })
   ],
+
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['es2015', 'react']
+        presets: ['es2015', 'react', 'stage-0']
       }
-    },{
+    }, {
       test: /\.json$/,
       loader: 'json'
     }]
