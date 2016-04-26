8821966fbced522c6ffbf7b184c9cd9e84885b17
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-04-26 23:20:01 +0100

    Setup Redux Storage

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,15 +1,17 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import * as storage from 'redux-storage';
 
 import auth from './auth';
 import notifications from './notifications';
 import searchFilter from './searchFilter';
 import settings from './settings';
 
-export default combineReducers({
+
+export default storage.reducer(combineReducers({
   auth,
   notifications,
   searchFilter,
   settings,
   routing: routerReducer
-});
+}));
--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -1,17 +1,26 @@
 import { createStore, applyMiddleware } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
 
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-localstorage';
+import filter from 'redux-storage-decorator-filter';
+
 import authentication from '../middleware/authentication';
+import constants from '../utils/constants';
 import settings from '../middleware/settings';
 import token from '../middleware/token';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
+  const engine = filter(createEngine(constants.STORAGE_KEY), ['settings']);
+  const storageMiddleware = storage.createMiddleware(engine);
+
   const createStoreWithMiddleware = applyMiddleware(
     token, // Should be passed before 'apiMiddleware'
     apiMiddleware,
     authentication,
-    settings
+    settings,
+    storageMiddleware
   )(createStore);
   return createStoreWithMiddleware(rootReducer, initialState);
 };
