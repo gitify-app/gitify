e50726608b911c7a01902cb8a743dbf25c9173ef
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-04-27 18:07:31 +0100

    Raise Native Notifications

--- src/js/utils/notifications.js
+++ src/js/utils/notifications.js
@@ -1,7 +1,9 @@
+const ipcRenderer = window.require('electron').ipcRenderer;
+
 export default {
   setup(notifications, settings) {
     // If there are no new notifications just stop there
-    if (!notifications) { return; }
+    if (!notifications.length) { return; }
 
     if (settings.playSound) {
       this.raiseSoundNotification(notifications);
@@ -13,10 +15,42 @@ export default {
   },
 
   raiseNativeNotification(notifications) {
-    console.log('raiseNativeNotification');
+    const newCount = notifications.length;
+    const title = (newCount === 1 ? 'Gitify - ' + notifications[0].full_name : 'Gitify');
+    const body = (newCount === 1 ? notifications[0].subject : 'You\'ve got ' + newCount + ' notifications.');
+
+    var icon;
+    if (newCount === 1) {
+      switch (notifications[0].type) {
+        case 'Issue':
+          icon = 'images/notifications/issue.png';
+          break;
+        case 'Commit':
+          icon = 'images/notifications/commit.png';
+          break;
+        case 'PullRequest':
+          icon = 'images/notifications/pull-request.png';
+          break;
+        default:
+          icon = 'images/notifications/gitify.png';
+      }
+    }
+
+    const nativeNotification = new Notification(title, {
+      body: body,
+      icon: icon || false,
+      silent: true
+    });
+
+    nativeNotification.onclick = function () {
+      ipcRenderer.send('reopen-window');
+    };
+
+    return nativeNotification;
   },
 
   raiseSoundNotification(notifications) {
-    console.log('raiseSoundNotification');
+    const audio = new Audio('sounds/digi.wav');
+    audio.play();
   }
 };
