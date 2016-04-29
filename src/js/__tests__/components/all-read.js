e9197d8ace1c823e3bdf7da99287abb2de3e4ee6
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-04-29 21:10:31 +0100

    Clean Up

--- src/js/__tests__/components/all-read.js
+++ src/js/__tests__/components/all-read.js
@@ -1,30 +1,20 @@
 jest.unmock('../../components/all-read');
 
 import React from 'react'; // eslint-disable-line no-unused-vars
-// import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
 describe('Test for Settings Component', function () {
 
   const AllRead = require('../../components/all-read').default;
 
-  // beforeEach(function () {
-    // Actions = require('../../actions/actions.js');
-    // SettingsStore = require('../../stores/settings.js');
-    // Settings = require('../../components/settings.js');
-  // });
+  beforeEach(function () {
+    // Pass
+  });
 
   it('Should render the <AllRead /> component', function () {
 
     const instance = TestUtils.renderIntoDocument(<AllRead />);
-    // const elementNode = ReactDOM.findDOMNode(instance);
-
     const heading = TestUtils.findRenderedDOMComponentWithTag(instance, 'h4');
-    // console.log('--------');
-    // console.log('--------');
-    // // console.log(heading);
-    // console.log('--------');
-    // console.log('--------');
 
     expect(heading).toBeDefined();
     expect(heading.textContent).toEqual('No new notifications.');
