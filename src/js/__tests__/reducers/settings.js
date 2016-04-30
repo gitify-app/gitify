387aa0305e6d49bdc2a463c0635710f0f988d7ea
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-04-30 22:05:22 +0100

    Test Settings Reducer

--- src/js/__tests__/reducers/settings.js
+++ src/js/__tests__/reducers/settings.js
@@ -1,42 +1,45 @@
 import { expect } from 'chai';
 import reducer from '../../reducers/settings';
-// import { UPDATE_SETTING } from '../../actions';
+import { UPDATE_SETTING } from '../../actions';
 
 describe('reducers/settings.js', () => {
+  const initialState = {
+    participating: false,
+    playSound: true,
+    showNotifications: true,
+    markOnClick: false,
+    openAtStartup: false
+  };
+
   it('should return the initial state', () => {
-    const initialState = {
-      participating: false,
-      playSound: true,
-      showNotifications: true,
-      markOnClick: false,
-      openAtStartup: false
-    };
 
     expect(reducer(undefined, {})).to.eql(initialState);
+
   });
 
-  // it('should handle UPDATE_SETTING', () => {
-  //   expect(
-  //     reducer([], {
-  //       type: types.ADD_TODO,
-  //       text: 'Run the tests'
-  //     })
-  //   ).toEqual({})
-  //
-  //   expect(
-  //     reducer(
-  //       [
-  //         {
-  //           text: 'Use Redux',
-  //           completed: false,
-  //           id: 0
-  //         }
-  //       ],
-  //       {
-  //         type: types.ADD_TODO,
-  //         text: 'Run the tests'
-  //       }
-  //     )
-  //   ).toEqual({})
-  // })
+  it('should handle UPDATE_SETTING', () => {
+
+    const actionParticipating = {
+      type: UPDATE_SETTING,
+      setting: 'participating',
+      value: true
+    };
+
+    expect(reducer(undefined, actionParticipating)).to.eql({
+      ...initialState,
+      participating: true
+    });
+
+    const actionOpenAtStartUp = {
+      type: UPDATE_SETTING,
+      setting: 'openAtStartup',
+      value: true
+    };
+
+    expect(reducer(undefined, actionOpenAtStartUp)).to.eql({
+      ...initialState,
+      openAtStartup: true
+    });
+
+  });
 });
