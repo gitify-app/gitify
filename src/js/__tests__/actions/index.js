d1005ddfca04909a59504ed88294983e6b22ddab
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-05-02 22:29:59 +0100

    Fix tests

--- src/js/__tests__/actions/index.js
+++ src/js/__tests__/actions/index.js
@@ -12,7 +12,8 @@ describe('actions/index.js', () => {
     nock.cleanAll();
   });
 
-  it('should login a user', () => {
+  it('should login a user with success', () => {
+    const code = 'THISISACODE';
 
     nock('https://github.com/')
       .post('/login/oauth/access_token', {
@@ -22,11 +23,35 @@ describe('actions/index.js', () => {
         body: { access_token: 'THISISATOKEN' }
       });
 
+    const expectedActions = [
+      { type: actions.LOGIN_REQUEST, payload: undefined, meta: undefined },
+      { type: actions.LOGIN_SUCCESS, payload: { body: { access_token: 'THISISATOKEN' } }, meta: undefined }
+    ];
+
+    const store = createMockStore({ response: [] }, expectedActions);
+
+    return store.dispatch(actions.loginUser(code))
+      .then(() => { // return of async actions
+        expect(store.getActions()).to.eql(expectedActions);
+      });
+
+  });
+
+  it('should login a user with failure', () => {
     const code = 'THISISACODE';
+    const message = 'Oops! Something went wrong.';
+
+    nock('https://github.com/')
+      .post('/login/oauth/access_token', {
+
+      })
+      .reply(400, {
+        body: { message }
+      });
 
     const expectedActions = [
-      { type: actions.LOGIN_REQUEST, code },
-      { type: actions.LOGIN_SUCCESS, body: { access_token: 'THISISATOKEN' } }
+      { type: actions.LOGIN_REQUEST, payload: undefined, meta: undefined },
+      { type: actions.LOGIN_FAILURE, payload: { body: { message } }, error: true, meta: undefined }
     ];
 
     const store = createMockStore({ response: [] }, expectedActions);
