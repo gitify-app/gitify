e91e8dcd1e4390cc3d82c9edbaa37c686c539881
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-08-27 01:02:54 +0100

    167 Release notifications

--- src/js/__tests__/utils/helpers.js
+++ src/js/__tests__/utils/helpers.js
@@ -26,4 +26,30 @@ describe('utils/helpers.js', () => {
     expect(ipcRenderer.send).to.have.been.calledWith('update-icon', 'TrayIdle');
 
   });
+
+
+  it('should generate the GitHub url (issue)', () => {
+
+    var apiUrl = 'https://api.github.com/repos/ekonstantinidis/notifications-test/issues/3';
+    var newUrl = Helpers.generateGitHubUrl(apiUrl);
+    expect(newUrl).to.equal('https://www.github.com/ekonstantinidis/notifications-test/issues/3');
+
+  });
+
+  it('should generate the GitHub url (repos)', () => {
+
+    var apiUrl = 'https://api.github.com/repos/ekonstantinidis/notifications-test/pulls/12345';
+    var newUrl = Helpers.generateGitHubUrl(apiUrl);
+    expect(newUrl).to.equal('https://www.github.com/ekonstantinidis/notifications-test/pull/12345');
+
+  });
+
+  it('should generate the GitHub url (release)', () => {
+
+    var apiUrl = 'https://api.github.com/repos/ekonstantinidis/notifications-test/releases/3988077';
+    var newUrl = Helpers.generateGitHubUrl(apiUrl);
+    expect(newUrl).to.equal('https://www.github.com/ekonstantinidis/notifications-test/releases');
+
+  });
+
 });
--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -11,9 +11,16 @@ export default {
 
   generateGitHubUrl(url) {
     var newUrl = url.replace('api.github.com/repos', 'www.github.com');
+
     if (newUrl.indexOf('/pulls/') !== -1) {
       newUrl = newUrl.replace('/pulls/', '/pull/');
     }
+
+    if (newUrl.indexOf('/releases/') !== -1) {
+      newUrl = newUrl.replace('/repos', '');
+      newUrl = newUrl.substr(0, newUrl.lastIndexOf('/'));
+    }
+
     return newUrl;
   }
 };
