0aea45421572a7b5728369ac698b513a15817d66
Emmanouil Konstantinidis <hello@manos.im>
2016-12-28 23:36:27 +0000

    Switch react-toggle for react-icheck

--- src/js/components/settings-modal.js
+++ src/js/components/settings-modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Modal from 'react-modal';
-import Toggle from 'react-toggle';
+import { Checkbox } from 'react-icheck';
 
 const ipcRenderer = window.require('electron').ipcRenderer;
 
@@ -9,21 +9,17 @@ import { fetchNotifications, updateSetting, logout, toggleSettingsModal } from '
 
 const modalStyles = {
   overlay : {
-    backgroundColor   : 'rgba(255, 255, 255, 0.95)',
     zIndex            : 9999
   },
   content : {
     position                   : 'absolute',
-    top                        : '50px',
-    left                       : '25px',
-    right                      : '25px',
-    bottom                     : '50px',
-    border                     : '1px solid #ccc',
-    background                 : '#fff',
+    top                        : '0',
+    left                       : '0',
+    right                      : '0',
+    bottom                     : '0',
     overflow                   : 'auto',
     WebkitOverflowScrolling    : 'touch',
     outline                    : 'none',
-    padding                    : '20px'
   }
 };
 
@@ -55,12 +51,11 @@ export class SettingsModal extends React.Component {
   }
 
   render() {
-    const appVersion = require('../../../package.json').version;
     const { settings, isOpen } = this.props;
 
     return (
       <Modal
-        className="settings"
+        className="settings bg-inverse"
         isOpen={isOpen}
         style={modalStyles}
         contentLabel="Settings Modal">
@@ -72,55 +67,50 @@ export class SettingsModal extends React.Component {
         </h3>
 
         <div className="row setting">
-          <div className="col-xs-8">Show only participating</div>
-          <div className="col-xs-4">
-            <Toggle
-              defaultChecked={settings.get('participating')}
-              onChange={(evt) => this.props.updateSetting('participating', evt.target.checked)} />
-          </div>
+          <Checkbox
+            label="Show only participating"
+            checkboxClass="icheckbox_polaris"
+            defaultChecked={settings.get('participating')}
+            onChange={(evt) => this.props.updateSetting('participating', evt.target.checked)} />
         </div>
 
         <div className="row setting">
-          <div className="col-xs-8">Play sound</div>
-          <div className="col-xs-4">
-            <Toggle
-              defaultChecked={settings.get('playSound')}
-              onChange={(evt) => this.props.updateSetting('playSound', evt.target.checked)} />
-          </div>
+          <Checkbox
+            label="Play sound"
+            checkboxClass="icheckbox_polaris"
+            defaultChecked={settings.get('playSound')}
+            onChange={(evt) => this.props.updateSetting('playSound', evt.target.checked)} />
         </div>
 
         <div className="row setting">
-          <div className="col-xs-8">Show notifications</div>
-          <div className="col-xs-4">
-            <Toggle
-              defaultChecked={settings.get('showNotifications')}
-              onChange={(evt) => this.props.updateSetting('showNotifications', evt.target.checked)} />
-          </div>
+          <Checkbox
+            label="Show notifications"
+            checkboxClass="icheckbox_polaris"
+            defaultChecked={settings.get('showNotifications')}
+            onChange={(evt) => this.props.updateSetting('showNotifications', evt.target.checked)} />
         </div>
 
         <div className="row setting">
-          <div className="col-xs-8">On Click, Mark as Read</div>
-          <div className="col-xs-4">
-            <Toggle
-              defaultChecked={settings.get('markOnClick')}
-              onChange={(evt) => this.props.updateSetting('markOnClick', evt.target.checked)} />
-          </div>
+          <Checkbox
+            label="On Click, Mark as Read"
+            checkboxClass="icheckbox_polaris"
+            defaultChecked={settings.get('markOnClick')}
+            onChange={(evt) => this.props.updateSetting('markOnClick', evt.target.checked)} />
         </div>
 
         <div className="row setting">
-          <div className="col-xs-8">Open at startup</div>
-          <div className="col-xs-4">
-            <Toggle
-              defaultChecked={settings.get('openAtStartup')}
-              onChange={(evt) => this.props.updateSetting('openAtStartup', evt.target.checked)} />
-          </div>
+          <Checkbox
+            label="Open at startup"
+            checkboxClass="icheckbox_polaris"
+            defaultChecked={settings.get('openAtStartup')}
+            onChange={(evt) => this.props.updateSetting('openAtStartup', evt.target.checked)} />
         </div>
 
 
-        <button className="btn btn-block btn-outline-danger" onClick={() => this.logout()}>
-          <span className="octicon octicon-sign-out" /> Logout
-        </button>
-      </Modal>
+      <button className="btn btn-block btn-outline-danger btn-logout" onClick={() => this.logout()}>
+        <span className="octicon octicon-sign-out" /> Logout
+      </button>
+    </Modal>
     );
   }
 };
