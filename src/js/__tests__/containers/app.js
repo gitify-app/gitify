1c0e0b0b6b3401d692bf6e81ab4e4376808b54a2
Emmanouil Konstantinidis <hello@manos.im>
2017-01-01 17:56:03 +0000

    Test app container

--- src/js/__tests__/containers/app.js
+++ src/js/__tests__/containers/app.js
@@ -1,6 +1,6 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { shallow } from 'enzyme';
-import { App } from '../../containers/app';
+import { App, mapStateToProps } from '../../containers/app';
 
 describe('containers/app.js', function () {
   it('should render itself & its children', function () {
@@ -10,5 +10,21 @@ describe('containers/app.js', function () {
     const wrapper = shallow(<App {...props} />);
 
     expect(wrapper).toBeDefined();
+  });
+
+  it('test the mapStateToProps function', () => {
+    const state = {
+      isFetching: false,
+      showSettingsModal: true
+    };
+
+    const ownProps = {
+      showId: 1234
+    };
+
+    const props = mapStateToProps(state, ownProps);
+
+    expect(props.isFetching).toBeFalsy();
+    expect(props.showSettingsModal).toBeTruthy();
   });
 });
