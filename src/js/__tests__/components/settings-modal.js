672442c4482cf69aabbdf2e4910119a01f155645
Emmanouil Konstantinidis <hello@manos.im>
2017-02-18 19:56:20 +0000

    Test settings modal

--- src/js/__tests__/components/settings-modal.js
+++ src/js/__tests__/components/settings-modal.js
@@ -5,7 +5,7 @@ import { Checkbox, RadioGroup, Radio } from 'react-icheck';
 
 const { ipcRenderer } = require('electron');
 
-import { SettingsModal } from '../../components/settings-modal';
+import { SettingsModal, mapStateToProps } from '../../components/settings-modal';
 
 const options = {
   context: {
@@ -51,10 +51,22 @@ describe('components/settings-modal.js', () => {
   beforeEach(function() {
     ipcRenderer.send.mockReset();
     props.updateSetting.mockReset();
+    props.toggleSettingsModal.mockReset();
   });
 
+  it('should test the mapStateToProps method', () => {
+    const state = {
+      settings: {
+        hello: 'world'
+      }
     };
 
+    const mappedProps = mapStateToProps(state);
+
+    expect(mappedProps.settings.hello).toEqual('world');
+  });
+
+  it('should render itself & its children', () => {
     const { wrapper } = setup(props);
     expect(wrapper).toBeDefined();
     expect(wrapper.find(Checkbox).length).toBe(5);
@@ -63,20 +75,7 @@ describe('components/settings-modal.js', () => {
     expect(wrapper.find('.octicon-sign-out').length).toBe(1);
   });
 
-  it('should update a setting', function () {
-
-    expect(wrapper).toBeDefined();
-
-
-
-    wrapper.setProps({
-      ...props,
-    });
-    expect(props.fetchNotifications).toHaveBeenCalledTimes(1);
-
-  });
-
-
+  it('should press the logout', () => {
     const { wrapper, context } = setup(props);
 
     expect(wrapper).toBeDefined();
@@ -94,4 +93,92 @@ describe('components/settings-modal.js', () => {
     expect(props.toggleSettingsModal).toHaveBeenCalledTimes(1);
   });
 
+  it('should call the componentWillReceiveProps method', function () {
+    const { wrapper } = setup(props);
+    expect(wrapper).toBeDefined();
+
+    wrapper.setProps({
+      ...props,
+      settings: props.settings.set('participating', !props.settings.get('participating'))
+    });
+    expect(props.fetchNotifications).toHaveBeenCalledTimes(1);
+  });
+
+  it('should close the modal pressing the icon', () => {
+    const { wrapper } = setup(props);
+    expect(wrapper).toBeDefined();
+
+    wrapper
+      .findWhere(node => node.props().testID === 'closeIcon')
+      .simulate('click');
+
+    expect(props.toggleSettingsModal).toHaveBeenCalledTimes(1);
+  });
+
+  it('should toggle the showOnlyParticipating checbox', () => {
+    const { wrapper } = setup(props);
+    expect(wrapper).toBeDefined();
+
+    wrapper
+      .findWhere(node => node.props().testID === 'showOnlyParticipating')
+      .simulate('change', {target: {checked: true}});
+
+    expect(props.updateSetting).toHaveBeenCalledTimes(1);
+  });
+
+  it('should toggle the playSound checbox', () => {
+    const { wrapper } = setup(props);
+    expect(wrapper).toBeDefined();
+
+    wrapper
+      .findWhere(node => node.props().testID === 'playSound')
+      .simulate('change', {target: {checked: true}});
+
+    expect(props.updateSetting).toHaveBeenCalledTimes(1);
+  });
+
+  it('should toggle the showNotifications checbox', () => {
+    const { wrapper } = setup(props);
+    expect(wrapper).toBeDefined();
+
+    wrapper
+      .findWhere(node => node.props().testID === 'showNotifications')
+      .simulate('change', {target: {checked: true}});
+
+    expect(props.updateSetting).toHaveBeenCalledTimes(1);
+  });
+
+  it('should toggle the onClickMarkAsRead checbox', () => {
+    const { wrapper } = setup(props);
+    expect(wrapper).toBeDefined();
+
+    wrapper
+      .findWhere(node => node.props().testID === 'onClickMarkAsRead')
+      .simulate('change', {target: {checked: true}});
+
+    expect(props.updateSetting).toHaveBeenCalledTimes(1);
+  });
+
+  it('should toggle the openAtStartup checbox', () => {
+    const { wrapper } = setup(props);
+    expect(wrapper).toBeDefined();
+
+    wrapper
+      .findWhere(node => node.props().testID === 'openAtStartup')
+      .simulate('change', {target: {checked: true}});
+
+    expect(props.updateSetting).toHaveBeenCalledTimes(1);
+  });
+
+  it('should toggle the showAppIcon radiogroup', () => {
+    const { wrapper } = setup(props);
+    expect(wrapper).toBeDefined();
+
+    wrapper
+      .findWhere(node => node.props().testID === 'showAppIcon')
+      .simulate('change', {target: {value: 'both'}});
+
+    expect(props.updateSetting).toHaveBeenCalledTimes(1);
+    expect(props.updateSetting).toHaveBeenCalledWith('showAppIcon', 'both');
+  });
 });
