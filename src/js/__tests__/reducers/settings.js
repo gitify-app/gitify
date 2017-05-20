81cd08cbdd9f4d9a265fd04b736d12d724dd5cf8
Emmanouil Konstantinidis <hello@manos.im>
2017-05-20 20:17:03 +0100

    Start fixing tests

--- src/js/__tests__/reducers/settings.js
+++ src/js/__tests__/reducers/settings.js
@@ -1,26 +1,9 @@
-import { Map } from 'immutable';
-
 import reducer from '../../reducers/settings';
 import { UPDATE_SETTING, HAS_STARRED, TOGGLE_SETTINGS_MODAL } from '../../actions';
 
 describe('reducers/settings.js', () => {
-  const initialState = Map({
-    participating: false,
-    playSound: true,
-    showNotifications: true,
-    markOnClick: false,
-    openAtStartup: false,
-    showSettingsModal: false,
-    hasStarred: false,
-    showAppIcon: 'both',
-    isEnterprise: false,
-    baseUrl: 'github.com',
-    clientId: '3fef4433a29c6ad8f22c',
-    clientSecret: '9670de733096c15322183ff17ed0fc8704050379'
-  });
-
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(initialState);
+    expect(reducer(undefined, {})).toMatchSnapshot();
   });
 
   it('should handle UPDATE_SETTING', () => {
@@ -30,10 +13,7 @@ describe('reducers/settings.js', () => {
       value: true
     };
 
-    expect(reducer(undefined, actionParticipating)).toEqual(
-      initialState
-        .set('participating', true)
-    );
+    expect(reducer(undefined, actionParticipating)).toMatchSnapshot();
 
     const actionOpenAtStartUp = {
       type: UPDATE_SETTING,
@@ -41,32 +21,23 @@ describe('reducers/settings.js', () => {
       value: true
     };
 
-    expect(reducer(undefined, actionOpenAtStartUp)).toEqual(
-      initialState
-        .set('openAtStartup', true)
-    );
+    expect(reducer(undefined, actionOpenAtStartUp)).toMatchSnapshot();
   });
 
   it('should handle HAS_STARRED.SUCCESS', () => {
-    const actionParticipating = {
+    const action = {
       type: HAS_STARRED.SUCCESS
     };
 
-    expect(reducer(undefined, actionParticipating)).toEqual(
-      initialState
-        .set('hasStarred', true)
-    );
+    expect(reducer(undefined, action)).toMatchSnapshot();
   });
 
   it('should handle HAS_STARRED.SUCCESS', () => {
-    const actionParticipating = {
+    const action = {
       type: HAS_STARRED.FAILURE
     };
 
-    expect(reducer(undefined, actionParticipating)).toEqual(
-      initialState
-        .set('hasStarred', false)
-    );
+    expect(reducer(undefined, action)).toMatchSnapshot();
   });
 
   it('should handle TOGGLE_SETTINGS_MODAL', () => {
@@ -74,9 +45,6 @@ describe('reducers/settings.js', () => {
       type: TOGGLE_SETTINGS_MODAL
     };
 
-    expect(reducer(undefined, action)).toEqual(
-      initialState
-        .set('showSettingsModal', true)
-    );
+    expect(reducer(undefined, action)).toMatchSnapshot();
   });
 });
