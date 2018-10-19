0f3ad7407b9091e3670940bf79d535c8e7d3a3d5
vzvu3k6k <vzvu3k6k@gmail.com>
2018-10-20 00:27:13 +0900

    Add button

--- src/js/__tests__/components/notifications.js
+++ src/js/__tests__/components/notifications.js
@@ -1,8 +1,9 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import { List } from 'immutable';
 
-import AccountNotifications from '../../components/notifications';
+import { AccountNotifications } from '../../components/notifications';
 import { mockedGithubNotifications } from '../../__mocks__/mockedData';
 
 jest.mock('../../components/repository');
@@ -12,6 +13,7 @@ describe('components/notifications.js', () => {
     const props = {
       hostname: 'github.com',
       notifications: mockedGithubNotifications,
+      markAccountNotifications: () => {},
     };
 
     const tree = renderer.create(<AccountNotifications {...props} />);
@@ -22,9 +24,24 @@ describe('components/notifications.js', () => {
     const props = {
       hostname: 'github.com',
       notifications: List(),
+      markAccountNotifications: () => {},
     };
 
     const tree = renderer.create(<AccountNotifications {...props} />);
     expect(tree).toMatchSnapshot();
+  });
+
+  it("should mark an account's notification as read", () => {
+    const props = {
+      markAccountNotifications: jest.fn(),
+      hostname: 'github.com',
+      notifications: mockedGithubNotifications,
+    };
+
+    const wrapper = shallow(<AccountNotifications {...props} />);
+
+    expect(wrapper).toBeDefined();
+    wrapper.find('.octicon-check').simulate('click');
+    expect(props.markAccountNotifications).toHaveBeenCalledTimes(1);
   });
 });
--- src/js/components/notifications.js
+++ src/js/components/notifications.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import RepositoryNotifications from './repository';
 
-export default class AccountNotifications extends React.Component {
+import { markAccountNotifications } from '../actions';
+
+export class AccountNotifications extends React.Component {
   static propTypes = {
     hostname: PropTypes.string.isRequired,
     notifications: PropTypes.any.isRequired,
+    markAccountNotifications: PropTypes.func.isRequired,
+  };
+
+  markAllAsRead = () => {
+    this.props.markAccountNotifications(this.props.hostname);
   };
 
   render() {
@@ -25,11 +33,13 @@ export default class AccountNotifications extends React.Component {
       >
         <div className="account p-2">
           {hostname}
-          <span
-            className={`octicon octicon-chevron-${notifications.isEmpty()
-              ? 'left'
-              : 'down'} ml-2`}
-          />
+          {notifications.isEmpty()
+            ? <span className="octicon octicon-chevron-left ml-2" />
+            : <span
+                title="Mark All as Read"
+                className="octicon octicon-check ml-2"
+                onClick={() => this.markAllAsRead()}
+              />}
         </div>
 
         {groupedNotifications.valueSeq().map(obj => {
@@ -47,3 +57,20 @@ export default class AccountNotifications extends React.Component {
     );
   }
 }
+
+export function mapStateToProps(state) {
+  return {
+    markOnClick: state.settings.get('markOnClick'),
+  };
+}
+
+export default connect(null, dispatch => ({
+  markAccountNotifications: hostname => {
+    const confirmed = confirm(
+      'Are you sure you want to mark all unread notifications as read?'
+    );
+    if (confirmed) {
+      dispatch(markAccountNotifications(hostname));
+    }
+  },
+}))(AccountNotifications);
