4989b095c5ce55a545453fd4fc6c3fcddd0c8355
Emmanouil Konstantinidis <hello@manos.im>
2020-12-12 00:22:08 +0000

    chore: Setup TailwindCSS

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import 'tailwindcss/tailwind.css';
+
 import { App } from './app';
 
 ReactDOM.render(<App />, document.getElementById('gitify'));
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#203354',
+        success: '#2CC966',
+        info: '#8BA9C6',
+        warning: '#FCAA67',
+        danger: '#B7524F',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
