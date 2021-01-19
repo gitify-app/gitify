09250bdc3aae67020e663bcc171979b5100e12ff
Shubhendra Singh Chauhan <withshubh@gmail.com>
2021-01-19 11:58:00 +0000

    Replace variables with const

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -18,7 +18,7 @@ export const useInterval = (callback, delay) => {
     }
 
     if (delay !== null) {
-      let id = setInterval(tick, delay);
+      const id = setInterval(tick, delay);
       return () => clearInterval(id);
     }
   }, [delay]);
