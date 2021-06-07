a33011a64f804c4d6d3408d82b1387cabcf91da2
송민욱 <musong@pubg.com>
2021-06-08 02:34:48 +0900

    Fix validateToken to support enterprise server

--- src/context/App.tsx
+++ src/context/App.tsx
@@ -21,6 +21,7 @@ import { setAppearance } from '../utils/appearance';
 import { setAutoLaunch } from '../utils/comms';
 import { useInterval } from '../hooks/useInterval';
 import { useNotifications } from '../hooks/useNotifications';
+import {generateGitHubAPIUrl} from "../utils/helpers";
 
 const defaultAccounts: AuthState = {
   token: null,
@@ -127,17 +128,14 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   );
 
   const validateToken = useCallback(
-    async ({ token, hostname }: AuthTokenOptions) => {
-      await apiRequestAuth(
-        `https://api.${hostname}/notifications`,
-        'HEAD',
-        token
-      );
-      const updatedAccounts = addAccount(accounts, token, hostname);
-      setAccounts(updatedAccounts);
-      saveState(updatedAccounts, settings);
-    },
-    [accounts, settings]
+   async ({token, hostname}: AuthTokenOptions) => {
+     const url = generateGitHubAPIUrl(hostname) + "notifications";
+     await apiRequestAuth(url, 'HEAD', token);
+     const updatedAccounts = addAccount(accounts, token, hostname);
+     setAccounts(updatedAccounts);
+     saveState(updatedAccounts, settings);
+   },
+   [accounts, settings]
   );
 
   const logout = useCallback(() => {
