8869d69aee8b20dab0666a6aef2daa6a6bbd5c82
Araxeus <78568641+Araxeus@users.noreply.github.com>
2022-03-28 20:48:59 +0300

    Platform specific native notification title

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -69,7 +69,7 @@ export const raiseNativeNotification = (
 
   if (notifications.length === 1) {
     const notification = notifications[0];
-    title = notification.repository.full_name;
+    title = `${process.platform !== 'win32' ? 'Gitify - ' : ''}${notification.repository.full_name}`;
     body = notification.subject.title;
     notificationUrl = notification.subject.url;
   } else {
