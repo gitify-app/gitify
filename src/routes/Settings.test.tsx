473b057721a1df2ccf260a69a265838a02671f47
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2023-10-17 23:13:10 +0100

    git stash

--- src/routes/Settings.test.tsx
+++ src/routes/Settings.test.tsx
@@ -10,6 +10,7 @@ const { ipcRenderer } = require('electron');
 import { SettingsRoute } from './Settings';
 import { AppContext } from '../context/App';
 import { mockAccounts, mockSettings } from '../__mocks__/mock-state';
+import Constants from '../utils/constants';
 
 const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -300,6 +301,60 @@ describe('routes/Settings.tsx', () => {
 
     fireEvent.click(getByLabelText('Quit Gitify'));
     expect(ipcRenderer.send).toHaveBeenCalledWith('app-quit');
+  });
+
+  it('should be able to enable colors', async () => {
+    let getByLabelText;
+    jest.mock('../utils/api-requests', () => ({
+      ...jest.requireActual('../utils/api-requests'),
+      apiRequestAuth: jest.fn().mockResolvedValue({
+        headers: {
+          'x-oauth-scopes': Constants.AUTH_SCOPE.join(', '),
+          'access-control-allow-headers': 'Authorization',
+          'access-control-allow-origin': '*',
+          'access-control-expose-headers': 'X-OAuth-Scopes',
+          'cache-control': 'private, max-age=60, s-maxage=60',
+          'content-encoding': 'gzip',
+          'content-security-policy': "default-src 'none'",
+          'content-type': 'application/json; charset=utf-8',
+          server: 'GitHub.com',
+          'strict-transport-security':
+            'max-age=31536000; includeSubdomains; preload',
+          vary: 'Accept, Authorization, Cookie, X-GitHub-OTP, Accept-Encoding, Accept, X-Requested-With',
+        },
+      }),
+    }));
+    await act(async () => {
+      const { getByLabelText: getByLabelTextLocal } = render(
+        <AppContext.Provider
+          value={{
+            settings: mockSettings,
+            accounts: mockAccounts,
+            updateSetting,
+          }}
+        >
+          <MemoryRouter>
+            <SettingsRoute />
+          </MemoryRouter>
+        </AppContext.Provider>,
+      );
+      getByLabelText = getByLabelTextLocal;
+    });
+
+    // await act(async () => {
+    //   expect(getByLabelText('Use GitHub-like state colors')).toBeDefined();
+    // });
+
+    // await act(async () => {
+    await act(
+      () =>
+        // waitFor(() =>
+        fireEvent.click(getByLabelText('Use GitHub-like state colors')),
+      // ),
+    );
+
+    expect(updateSetting).toHaveBeenCalledTimes(1);
+    expect(updateSetting).toHaveBeenCalledWith('colors', true);
   });
 
   it('should not be able to disable colors', async () => {
