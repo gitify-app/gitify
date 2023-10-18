a5e6b27c1689c136f55957f212c8db2fde27201c
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2023-10-18 01:42:31 +0100

    chore: remove `emojione`

--- src/components/AllRead.tsx
+++ src/components/AllRead.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { emojify } from 'react-emojione';
 
 import { Constants } from '../utils/constants';
 
@@ -14,7 +13,7 @@ export const AllRead = () => {
 
   return (
     <div className="flex flex-1 flex-col justify-center items-center p-4 bg-white dark:bg-gray-dark text-black dark:text-white">
-      <h1 className="text-5xl mb-5">{emojify(emoji, { output: 'unicode' })}</h1>
+      <h1 className="text-5xl mb-5">{emoji}</h1>
 
       <h2 className="font-semibold text-xl mb-2 text-semibold">
         No new notifications.
