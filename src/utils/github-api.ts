1db955316ad2f12e9ff9f005f6b4e82a6fd1e3b2
Adam Setch <adam.setch@outlook.com>
2024-03-05 17:39:38 -0500

    feat: simplify icon colors and improve accessibility when in light-mode (#857)
    * feat: simplify icon colors and improve accessibility when in light-mode
    
    * feat: change gray color based on theme

--- src/utils/github-api.ts
+++ src/utils/github-api.ts
@@ -25,6 +25,8 @@ import {
   XIcon,
 } from '@primer/octicons-react';
 import { Reason, Subject } from '../typesGithub';
+import { Appearance } from '../types';
+import { getAppearance } from './appearance';
 
 // prettier-ignore
 const DESCRIPTIONS = {
@@ -157,33 +159,23 @@ export function getNotificationTypeIcon(
 
 export function getNotificationTypeIconColor(subject: Subject): string {
   switch (subject.state) {
-    case 'ANSWERED':
-      return 'text-green-500';
-    case 'cancelled':
-      return 'text-gray-500';
-    case 'closed':
-      return 'text-red-500';
-    case 'completed':
-      return 'text-purple-500';
-    case 'draft':
-      return 'text-gray-600';
-    case 'failure':
-      return 'text-red-500';
-    case 'merged':
-      return 'text-purple-500';
-    case 'not_planned':
-      return 'text-gray-300';
     case 'open':
-      return 'text-green-500';
     case 'reopened':
-      return 'text-green-500';
-    case 'RESOLVED':
-      return 'text-purple-500';
-    case 'skipped':
-      return 'text-gray-500';
+    case 'ANSWERED':
     case 'success':
       return 'text-green-500';
+    case 'closed':
+    case 'failure':
+      return 'text-red-500';
+    case 'completed':
+    case 'RESOLVED':
+    case 'merged':
+      return 'text-purple-500';
     default:
-      return 'text-gray-300';
+      const appearance = getAppearance();
+      if (appearance == Appearance.DARK) {
+        return 'text-gray-300';
+      }
+      return 'text-gray-500';
   }
 }
