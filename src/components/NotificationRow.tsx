21c01fed39e125d85eb4e88f149d59c32b0f3a68
Adam Setch <adam.setch@outlook.com>
2024-03-08 14:32:48 -0500

    feat: show action buttons only on hover

--- src/components/NotificationRow.tsx
+++ src/components/NotificationRow.tsx
@@ -63,7 +63,7 @@ export const NotificationRow: React.FC<IProps> = ({
   });
 
   return (
-    <div className="flex space-x-3 py-2 px-3 bg-white dark:bg-gray-dark dark:text-white hover:bg-gray-100 dark:hover:bg-gray-darker border-b border-gray-100 dark:border-gray-darker">
+    <div className="flex space-x-3 py-2 px-3 bg-white dark:bg-gray-dark dark:text-white hover:bg-gray-100 dark:hover:bg-gray-darker border-b border-gray-100 dark:border-gray-darker group">
       <div className={`flex justify-center items-center w-5 ${realIconColor}`}>
         <NotificationIcon size={18} aria-label={notification.subject.type} />
       </div>
@@ -83,7 +83,7 @@ export const NotificationRow: React.FC<IProps> = ({
         </div>
       </div>
 
-      <div className="flex justify-center items-center gap-2">
+      <div className="flex justify-center items-center gap-2 opacity-0 group-hover:opacity-80 transition-opacity">
         <button
           className="focus:outline-none h-full hover:text-green-500"
           title="Mark as Done"
