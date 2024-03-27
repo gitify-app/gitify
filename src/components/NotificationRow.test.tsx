8b0ce303365509e470e25f08094ede7ce00cdef8
Adam Setch <adam.setch@outlook.com>
2024-03-27 07:10:05 -0400

    refactor: use getByTitle for consistency

--- src/components/NotificationRow.test.tsx
+++ src/components/NotificationRow.test.tsx
@@ -138,14 +138,14 @@ describe('components/NotificationRow.tsx', () => {
       hostname: 'github.com',
     };
 
-    const { getByLabelText } = render(
+    const { getByTitle } = render(
       <AppContext.Provider value={{}}>
         <AppContext.Provider value={{ unsubscribeNotification }}>
           <NotificationRow {...props} />
         </AppContext.Provider>
       </AppContext.Provider>,
     );
-    fireEvent.click(getByLabelText('Unsubscribe'));
+    fireEvent.click(getByTitle('Unsubscribe'));
     expect(unsubscribeNotification).toHaveBeenCalledTimes(1);
   });
 });
