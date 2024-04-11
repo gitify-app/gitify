03efad015cb8c285c9ed1e72cf12ae94cb665e2d
Adam Setch <adam.setch@outlook.com>
2024-04-11 10:12:24 -0400

    refactor: improve PAT login experience

--- src/components/fields/FieldInput.tsx
+++ src/components/fields/FieldInput.tsx
@@ -31,7 +31,7 @@ export const FieldInput: FC<IProps> = ({
 
           <input
             type={type}
-            className="appearance-none block w-full dark:text-gray-800 bg-gray-100 border border-red rounded py-1.5 px-4 mb-2 focus:bg-gray-200 focus:outline-none"
+            className="appearance-none block w-full dark:text-gray-800 bg-gray-100 border border-red rounded py-1.5 px-4 mb-2 focus:bg-gray-200 focus:outline-none text-sm"
             id={input.name}
             placeholder={placeholder}
             required={required}
