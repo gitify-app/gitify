76df125bc462bc347cb15331447f8a47baaa0d70
Adam Setch <adam.setch@outlook.com>
2024-04-18 09:14:35 -0400

    tests(subject): improve mocks

---
+++ src/__mocks__/partial-mocks.ts
@@ -0,0 +1,22 @@
+import type { Notification, Subject, User } from '../typesGithub';
+
+export function partialMockNotification(
+  subject: Partial<Subject>,
+): Notification {
+  const mockNotification: Partial<Notification> = {
+    subject: subject as Subject,
+  };
+
+  return mockNotification as Notification;
+}
+
+export function partialMockUser(login: string): User {
+  const mockUser: Partial<User> = {
+    login: login,
+    html_url: `https://github.com/${login}`,
+    avatar_url: 'https://avatars.githubusercontent.com/u/583231?v=4',
+    type: 'User',
+  };
+
+  return mockUser as User;
+}
