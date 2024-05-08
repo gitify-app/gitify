52ff048fd6c9c4c6d1023a0de1a7180944b1e18d
Adam Setch <adam.setch@outlook.com>
2024-05-07 22:41:54 -0400

    feat: delay notification removal

--- src/utils/remove-notification.test.ts
+++ src/utils/remove-notification.test.ts
@@ -3,6 +3,8 @@ import {
   mockedSingleAccountNotifications,
   mockedSingleNotification,
 } from '../__mocks__/mockedData';
+import Constants from './constants';
+// import Constants from './constants';
 import { removeNotification } from './remove-notification';
 
 describe('utils/remove-notification.ts', () => {
@@ -13,12 +15,33 @@ describe('utils/remove-notification.ts', () => {
     expect(mockedSingleAccountNotifications[0].notifications.length).toBe(1);
 
     const result = removeNotification(
-      mockSettings,
+      { ...mockSettings, delayNotificationState: false },
       notificationId,
       mockedSingleAccountNotifications,
       hostname,
     );
 
     expect(result[0].notifications.length).toBe(0);
+  });
+
+  it('should set notification as opaque if delayNotificationState enabled', () => {
+    const mockElement = document.createElement('div');
+    mockElement.id = mockedSingleAccountNotifications[0].notifications[0].id;
+    jest.spyOn(document, 'getElementById').mockReturnValue(mockElement);
+
+    expect(mockedSingleAccountNotifications[0].notifications.length).toBe(1);
+
+    const result = removeNotification(
+      { ...mockSettings, delayNotificationState: true },
+      notificationId,
+      mockedSingleAccountNotifications,
+      hostname,
+    );
+
+    expect(result[0].notifications.length).toBe(1);
+    expect(document.getElementById).toHaveBeenCalledWith(
+      mockedSingleAccountNotifications[0].notifications[0].id,
+    );
+    expect(mockElement.className).toContain(Constants.READ_CLASS_NAME);
   });
 });
