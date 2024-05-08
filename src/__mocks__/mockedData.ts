e41438a3699546574a38c821fd082860c1a2fa1f
Adam Setch <adam.setch@outlook.com>
2024-05-08 03:05:05 -1000

    refactor(discussions): use API to get latest comment and reply (#1094)

--- src/__mocks__/mockedData.ts
+++ src/__mocks__/mockedData.ts
@@ -405,44 +405,6 @@ const mockDiscussionReplier: DiscussionAuthor = {
 export const mockDiscussionComments: DiscussionComments = {
   nodes: [
     {
-      databaseId: 2215656,
-      createdAt: '2022-02-20T18:33:39Z',
-      author: mockDiscussionAuthor,
-      replies: {
-        nodes: [],
-      },
-    },
-    {
-      databaseId: 2217789,
-      createdAt: '2022-02-21T03:30:42Z',
-      author: mockDiscussionAuthor,
-      replies: {
-        nodes: [],
-      },
-    },
-    {
-      databaseId: 2223243,
-      createdAt: '2022-02-21T18:26:27Z',
-      author: mockDiscussionAuthor,
-      replies: {
-        nodes: [
-          {
-            databaseId: 2232922,
-            createdAt: '2022-02-23T00:57:58Z',
-            author: mockDiscussionReplier,
-          },
-        ],
-      },
-    },
-    {
-      databaseId: 2232921,
-      createdAt: '2022-02-23T00:57:49Z',
-      author: mockDiscussionAuthor,
-      replies: {
-        nodes: [],
-      },
-    },
-    {
       databaseId: 2258799,
       createdAt: '2022-02-27T01:22:20Z',
       author: mockDiscussionAuthor,
@@ -451,35 +413,6 @@ export const mockDiscussionComments: DiscussionComments = {
           {
             databaseId: 2300902,
             createdAt: '2022-03-05T17:43:52Z',
-            author: mockDiscussionReplier,
-          },
-        ],
-      },
-    },
-    {
-      databaseId: 2297637,
-      createdAt: '2022-03-04T20:39:44Z',
-      author: mockDiscussionAuthor,
-
-      replies: {
-        nodes: [
-          {
-            databaseId: 2300893,
-            createdAt: '2022-03-05T17:41:04Z',
-            author: mockDiscussionReplier,
-          },
-        ],
-      },
-    },
-    {
-      databaseId: 2299763,
-      createdAt: '2022-03-05T11:05:42Z',
-      author: mockDiscussionAuthor,
-      replies: {
-        nodes: [
-          {
-            databaseId: 2300895,
-            createdAt: '2022-03-05T17:41:44Z',
             author: mockDiscussionReplier,
           },
         ],
