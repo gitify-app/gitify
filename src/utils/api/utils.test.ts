113891a2c8e6a79cb22d59063734eff123f0ba50
Adam Setch <adam.setch@outlook.com>
2024-05-17 05:20:56 -0400

    feat: support discussions for github server

--- src/utils/api/utils.test.ts
+++ src/utils/api/utils.test.ts
@@ -1,7 +1,7 @@
-import { getGitHubAPIBaseUrl } from './utils';
+import { getGitHubAPIBaseUrl, getGitHubGraphQLUrl } from './utils';
 
 describe('utils/api/utils.ts', () => {
-  describe('generateGitHubAPIUrl', () => {
+  describe('getGitHubAPIBaseUrl', () => {
     it('should generate a GitHub API url - non enterprise', () => {
       const result = getGitHubAPIBaseUrl('github.com');
       expect(result.toString()).toBe('https://api.github.com/');
@@ -10,6 +10,18 @@ describe('utils/api/utils.ts', () => {
     it('should generate a GitHub API url - enterprise', () => {
       const result = getGitHubAPIBaseUrl('github.manos.im');
       expect(result.toString()).toBe('https://github.manos.im/api/v3/');
+    });
+  });
+
+  describe('generateGitHubGraphQLAPIUrl', () => {
+    it('should generate a GitHub GraphQL url - non enterprise', () => {
+      const result = getGitHubGraphQLUrl('github.com');
+      expect(result.toString()).toBe('https://api.github.com/graphql');
+    });
+
+    it('should generate a GitHub GraphQL url - enterprise', () => {
+      const result = getGitHubGraphQLUrl('github.manos.im');
+      expect(result.toString()).toBe('https://github.manos.im/api/graphql');
     });
   });
 });
