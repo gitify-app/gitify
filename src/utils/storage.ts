da89b0ed0ead21c29abeb3d5ecff72cad57514bf
Adam Setch <adam.setch@outlook.com>
2024-05-24 08:02:59 -0400

    refactor: introduce new account types

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,66 +1,21 @@
 import type { AuthAccounts, SettingsState } from '../types';
+import { migrateLegacyAccounts } from './auth/migration';
 import { Constants } from './constants';
 
-export const loadState = (): {
-  accounts?: AuthAccounts;
-  settings?: SettingsState;
-} => {
+export function loadState() {
   const existing = localStorage.getItem(Constants.STORAGE_KEY);
   const { auth: accounts, settings } = (existing && JSON.parse(existing)) || {};
 
   const migratedAccounts = migrateLegacyAccounts(accounts);
 
   return { accounts: migratedAccounts, settings };
-};
+}
 
-export const saveState = (
-  auth: AuthAccounts,
-  settings: SettingsState,
-): void => {
+export function saveState(auth: AuthAccounts, settings: SettingsState) {
   const settingsString = JSON.stringify({ auth, settings });
   localStorage.setItem(Constants.STORAGE_KEY, settingsString);
-};
+}
 
-export const clearState = (): void => {
+export function clearState() {
   localStorage.clear();
-};
-
-function migrateLegacyAccounts(accounts?: AuthAccounts): AuthAccounts {
-  const migratedAccounts = accounts?.accounts ?? [];
-
-  if (accounts === undefined) {
-    return undefined;
-  }
-
-  if (migratedAccounts.length > 0) {
-    return {
-      accounts: migratedAccounts,
-    };
-  }
-
-  if (accounts?.token) {
-    migratedAccounts.push({
-      hostname: Constants.DEFAULT_AUTH_OPTIONS.hostname,
-      platform: 'GitHub Cloud',
-      method: 'Personal Access Token',
-      token: accounts.token,
-      user: accounts.user,
-    });
-  }
-
-  if (accounts?.enterpriseAccounts) {
-    for (const legacyEnterpriseAccount of accounts.enterpriseAccounts) {
-      migratedAccounts.push({
-        hostname: legacyEnterpriseAccount.hostname,
-        platform: 'GitHub Enterprise Server',
-        method: 'OAuth App',
-        token: legacyEnterpriseAccount.token,
-        user: null,
-      });
-    }
-  }
-
-  return {
-    accounts: migratedAccounts,
-  };
 }
