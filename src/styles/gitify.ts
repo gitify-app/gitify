3e4f234ec1740de41bb5e863a7c92836b142e097
Adam Setch <adam.setch@outlook.com>
2024-06-10 08:39:07 -0400

    refactor: move custom styles into class

---
+++ src/styles/gitify.ts
@@ -0,0 +1,4 @@
+export const READ_NOTIFICATION_CLASS_NAME = 'opacity-50 dark:opacity-50';
+
+export const PILL_CLASS_NAME =
+  'rounded-full text-xss px-1 m-0.5 bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700';
