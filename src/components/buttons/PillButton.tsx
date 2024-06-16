0e70ada8d42d6e848f95067e3fabc78221113f8b
Adam Setch <adam.setch@outlook.com>
2024-06-16 16:43:32 -0400

    fix: regression in pill rendering

--- src/components/buttons/PillButton.tsx
+++ src/components/buttons/PillButton.tsx
@@ -1,6 +1,7 @@
 import type { Icon } from '@primer/octicons-react';
 import type { FC } from 'react';
 import type { IconColor } from '../../types';
+import { cn } from '../../utils/cn';
 
 export interface IPillButton {
   key?: string;
@@ -12,13 +13,18 @@ export interface IPillButton {
 
 export const PillButton: FC<IPillButton> = (props: IPillButton) => {
   return (
-    <button
-      title={props.title}
-      type="button"
-      className="flex gap-1 items-center m-0.5 rounded-full bg-gray-100 px-1 text-xss hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
-    >
-      <props.icon size={12} className={props.color} aria-label={props.title} />
-      {props.metric}
-    </button>
+    <span title={props.title}>
+      <button
+        type="button"
+        className="m-0.5 rounded-full bg-gray-100 px-1 text-xss hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <props.icon
+          size={12}
+          className={cn(props.metric && 'mr-1', props.color)}
+          aria-label={props.title}
+        />
+        {props.metric}
+      </button>
+    </span>
   );
 };
