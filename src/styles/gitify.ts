2163a499e9c629603a6539b90b58d01641ac0d82
Adam Setch <adam.setch@outlook.com>
2024-06-16 07:07:23 -0400

    fix: delay state

--- src/styles/gitify.ts
+++ src/styles/gitify.ts
@@ -3,5 +3,3 @@ export const BUTTON_CLASS_NAME =
 
 export const BUTTON_SIDEBAR_CLASS_NAME =
   'flex justify-evenly items-center bg-transparent border-0 w-full text-sm text-white my-1 py-2 cursor-pointer hover:text-gray-500 focus:outline-none disabled:text-gray-500 disabled:cursor-default';
-
-export const READ_NOTIFICATION_CLASS_NAME = 'opacity-50 dark:opacity-50';
--- src/utils/remove-notification.test.ts
+++ src/utils/remove-notification.test.ts
@@ -1,6 +1,5 @@
 import { mockSingleAccountNotifications } from '../__mocks__/notifications-mocks';
 import { mockSettings } from '../__mocks__/state-mocks';
-import { READ_NOTIFICATION_CLASS_NAME } from '../styles/gitify';
 import { mockSingleNotification } from './api/__mocks__/response-mocks';
 import { removeNotification } from './remove-notification';
 
@@ -17,11 +16,7 @@ describe('utils/remove-notification.ts', () => {
     expect(result[0].notifications.length).toBe(0);
   });
 
-  it('should set notification as opaque if delayNotificationState enabled', () => {
-    const mockElement = document.createElement('div');
-    mockElement.id = mockSingleAccountNotifications[0].notifications[0].id;
-    jest.spyOn(document, 'getElementById').mockReturnValue(mockElement);
-
+  it('should skip notification removal if delay state enabled', () => {
     expect(mockSingleAccountNotifications[0].notifications.length).toBe(1);
 
     const result = removeNotification(
@@ -31,9 +26,5 @@ describe('utils/remove-notification.ts', () => {
     );
 
     expect(result[0].notifications.length).toBe(1);
-    expect(document.getElementById).toHaveBeenCalledWith(
-      mockSingleAccountNotifications[0].notifications[0].id,
-    );
-    expect(mockElement.className).toContain(READ_NOTIFICATION_CLASS_NAME);
   });
 });
