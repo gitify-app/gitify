d6123f09f9162f4a0052f45134e9e4677ff81fd9
Adam Setch <adam.setch@outlook.com>
2024-06-18 14:47:37 -0400

    build(biome): enable noConsoleLog rule (#1268)
    * build(biome): enable noConsoleLog rule
    
    * extract into fns
    
    * extract into fns

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -3,6 +3,11 @@ const { notarize } = require('@electron/notarize');
 const packageJson = require('../package.json');
 const appBundleId = packageJson.build.appId;
 
+function logNotarizingProgress(msg) {
+  // biome-ignore lint/suspicious/noConsoleLog: log notarizing progress
+  console.log(`  • notarizing        ${msg}`);
+}
+
 const notarizeApp = async (context) => {
   const { electronPlatformName, appOutDir } = context;
   const appName = context.packager.appInfo.productFilename;
@@ -10,13 +15,13 @@ const notarizeApp = async (context) => {
   const shouldNotarize = process.env.NOTARIZE === 'true';
 
   if (!shouldNotarize || !isMacOS) {
-    console.log(
-      '  • notarizing      either should not notarize or not building for macOS',
+    logNotarizingProgress(
+      'either should not notarize or not building for macOS',
     );
     return;
   }
 
-  console.log('  • notarizing      started');
+  logNotarizingProgress('process started');
 
   return await notarize({
     appBundleId,
