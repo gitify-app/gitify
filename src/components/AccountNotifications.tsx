faf9d370195034b676c37cfc24f2b619f3430a6a
Adam Setch <adam.setch@outlook.com>
2024-06-19 12:02:16 -0400

    fix: account header alignment

--- src/components/AccountNotifications.tsx
+++ src/components/AccountNotifications.tsx
@@ -58,10 +58,10 @@ export const AccountNotifications: FC<IAccountNotifications> = (
     <>
       {showAccountHostname && (
         <div
-          className="group flex items-center justify-between bg-gray-300 px-3 py-2 text-sm font-semibold dark:bg-gray-darkest dark:text-white"
+          className="group flex justify-between bg-gray-300 px-3 py-2 text-sm font-semibold dark:bg-gray-darkest dark:text-white"
           onClick={toggleAccountNotifications}
         >
-          <div className="flex gap-3">
+          <div className="flex gap-3 items-center">
             <PlatformIcon type={account.platform} size={16} />
             <button
               type="button"
