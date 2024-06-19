7ffdee18a796fb0993819c94b664525fed4559e1
Adam Setch <adam.setch@outlook.com>
2024-06-19 18:10:17 -0400

    refactor: rename custom text size to xxs (#1277)

--- src/components/buttons/PillButton.tsx
+++ src/components/buttons/PillButton.tsx
@@ -15,7 +15,7 @@ export const PillButton: FC<IPillButton> = (props: IPillButton) => {
     <button
       title={props.title}
       type="button"
-      className="flex gap-1 items-center m-0.5 rounded-full bg-gray-100 px-1 text-xss hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
+      className="flex gap-1 items-center m-0.5 rounded-full bg-gray-100 px-1 text-xxs hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
     >
       <props.icon size={12} className={props.color} aria-label={props.title} />
       {props.metric}
