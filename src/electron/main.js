e1a1218e002d56dbc5fb5bec2b66065a8084cc83
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2024-06-20 02:32:19 +0100

    fix: use `path.resolve` for loading images

--- src/electron/main.js
+++ src/electron/main.js
@@ -8,12 +8,17 @@ const {
 const { menubar } = require('menubar');
 const { autoUpdater } = require('electron-updater');
 const { onFirstRunMaybe } = require('./first-run');
+const path = require('node:path');
 
 // TODO: Remove @electron/remote use - see #650
 require('@electron/remote/main').initialize();
 
-const idleIcon = `${__dirname}/../../assets/images/tray-idleTemplate.png`;
-const activeIcon = `${__dirname}/../../assets/images/tray-active.png`;
+const idleIcon = path.resolve(
+  `${__dirname}/../../assets/images/tray-idleTemplate.png`,
+);
+const activeIcon = path.resolve(
+  `${__dirname}/../../assets/images/tray-active.png`,
+);
 
 const browserWindowOpts = {
   width: 500,
