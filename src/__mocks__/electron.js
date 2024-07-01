7a6aa866abe084fe67b021615354dfa6e94f44f1
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2024-07-01 15:14:41 +0100

    chore: revert electron mock

--- src/__mocks__/electron.js
+++ src/__mocks__/electron.js
@@ -40,7 +40,7 @@ module.exports = {
         case 'get-platform':
           return Promise.resolve('darwin');
         case 'gitify:version':
-          return Promise.resolve('dev');
+          return Promise.resolve('0.0.1');
         default:
           return Promise.reject(new Error(`Unknown channel: ${channel}`));
       }
