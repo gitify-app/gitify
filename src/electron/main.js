ac3f6036fc58d0336be269d57de94807b0bc5267
Adam Setch <adam.setch@outlook.com>
2024-07-22 10:27:36 +1000

    feat: check for updates

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,195 +1,197 @@
 const {
-  ipcMain: ipc,
-  app,
-  nativeTheme,
-  globalShortcut,
-  Menu,
-} = require('electron/main');
-const { menubar } = require('menubar');
-const { autoUpdater } = require('electron-updater');
-const { onFirstRunMaybe } = require('./first-run');
-const path = require('node:path');
-const log = require('electron-log');
+	ipcMain: ipc,
+	app,
+	nativeTheme,
+	globalShortcut,
+	Menu,
+} = require("electron/main");
+const { menubar } = require("menubar");
+const { autoUpdater } = require("electron-updater");
+const { onFirstRunMaybe } = require("./first-run");
+const path = require("node:path");
+const log = require("electron-log");
 
 autoUpdater.logger = log;
 
 // TODO: Remove @electron/remote use - see #650
-require('@electron/remote/main').initialize();
+require("@electron/remote/main").initialize();
 
 const idleIcon = path.resolve(
-  `${__dirname}/../../assets/images/tray-idleTemplate.png`,
+	`${__dirname}/../../assets/images/tray-idleTemplate.png`,
 );
 const activeIcon = path.resolve(
-  `${__dirname}/../../assets/images/tray-active.png`,
+	`${__dirname}/../../assets/images/tray-active.png`,
 );
 
 const browserWindowOpts = {
-  width: 500,
-  height: 400,
-  minWidth: 500,
-  minHeight: 400,
-  resizable: false,
-  webPreferences: {
-    enableRemoteModule: true,
-    nodeIntegration: true,
-    contextIsolation: false,
-  },
+	width: 500,
+	height: 400,
+	minWidth: 500,
+	minHeight: 400,
+	resizable: false,
+	webPreferences: {
+		enableRemoteModule: true,
+		nodeIntegration: true,
+		contextIsolation: false,
+	},
 };
 
 const contextMenu = Menu.buildFromTemplate([
-  {
-    role: 'reload',
-  },
-  {
-    role: 'toggleDevTools',
-  },
-  { type: 'separator' },
-  {
-    label: 'Check for updates',
-    click: () => {
-      checkForUpdates();
-    },
-  },
-  {
-    label: 'Quit',
-    click: () => {
-      app.quit();
-    },
-  },
+	{
+		role: "reload",
+	},
+	{
+		role: "toggleDevTools",
+	},
+	{ type: "separator" },
+	{
+		label: "Check for updates",
+		click: () => {
+			checkForUpdates();
+		},
+	},
+	{
+		label: "Quit",
+		click: () => {
+			app.quit();
+		},
+	},
 ]);
 
 app.whenReady().then(async () => {
-  await onFirstRunMaybe();
+	await onFirstRunMaybe();
 
-  const mb = menubar({
-    icon: idleIcon,
-    index: `file://${__dirname}/index.html`,
-    browserWindow: browserWindowOpts,
-    preloadWindow: true,
-    showDockIcon: false,
-  });
+	const mb = menubar({
+		icon: idleIcon,
+		index: `file://${__dirname}/index.html`,
+		browserWindow: browserWindowOpts,
+		preloadWindow: true,
+		showDockIcon: false,
+	});
 
-  mb.on('ready', () => {
-    mb.app.setAppUserModelId('com.electron.gitify');
+	mb.on("ready", () => {
+		mb.app.setAppUserModelId("com.electron.gitify");
 
-    // Tray configuration
-    mb.tray.setIgnoreDoubleClickEvents(true);
-    mb.tray.on('right-click', (_event, bounds) => {
-      mb.tray.popUpContextMenu(contextMenu, { x: bounds.x, y: bounds.y });
-    });
+		// Tray configuration
+		mb.tray.setIgnoreDoubleClickEvents(true);
+		mb.tray.on("right-click", (_event, bounds) => {
+			mb.tray.popUpContextMenu(contextMenu, { x: bounds.x, y: bounds.y });
+		});
 
-    // Custom key events
-    mb.window.webContents.on('before-input-event', (event, input) => {
-      if (input.key === 'Escape') {
-        mb.window.hide();
-        event.preventDefault();
-      }
-    });
+		// Custom key events
+		mb.window.webContents.on("before-input-event", (event, input) => {
+			if (input.key === "Escape") {
+				mb.window.hide();
+				event.preventDefault();
+			}
+		});
 
-    // DevTools configuration
-    mb.window.webContents.on('devtools-opened', () => {
-      mb.window.setSize(800, 600);
-      mb.window.center();
-      mb.window.resizable = true;
-      mb.window.setAlwaysOnTop(true);
-    });
+		// DevTools configuration
+		mb.window.webContents.on("devtools-opened", () => {
+			mb.window.setSize(800, 600);
+			mb.window.center();
+			mb.window.resizable = true;
+			mb.window.setAlwaysOnTop(true);
+		});
 
-    mb.window.webContents.on('devtools-closed', () => {
-      const trayBounds = mb.tray.getBounds();
-      mb.window.setSize(browserWindowOpts.width, browserWindowOpts.height);
-      mb.positioner.move('trayCenter', trayBounds);
-      mb.window.resizable = false;
-    });
+		mb.window.webContents.on("devtools-closed", () => {
+			const trayBounds = mb.tray.getBounds();
+			mb.window.setSize(browserWindowOpts.width, browserWindowOpts.height);
+			mb.positioner.move("trayCenter", trayBounds);
+			mb.window.resizable = false;
+		});
 
-    // Auto Update
-    checkForUpdates();
-    setInterval(checkForUpdates, 24 * 60 * 60 * 1000); // 24 hours
+		// Auto Update
+		checkForUpdates();
+		setInterval(checkForUpdates, 24 * 60 * 60 * 1000); // 24 hours
 
-    autoUpdater.on('update-available', (info) => {
-      mb.window.webContents.send(
-        'gitify:version-check',
-        'UPDATE_AVAILABLE',
-        info,
-      );
-    });
+		autoUpdater.on("update-available", (info) => {
+			log.info("Auto Updater: New update available", info);
+			mb.window.webContents.send(
+				"gitify:version-check",
+				"UPDATE_AVAILABLE",
+				info,
+			);
+		});
 
-    autoUpdater.on('update-not-available', (info) => {
-      mb.window.webContents.send(
-        'gitify:version-check',
-        'UPDATE_NOT_AVAILABLE',
-        info,
-      );
-    });
+		autoUpdater.on("update-not-available", (info) => {
+			log.info("Auto Updater: Already on the latest version");
+			mb.window.webContents.send(
+				"gitify:version-check",
+				"UPDATE_NOT_AVAILABLE",
+				info,
+			);
+		});
 
-    autoUpdater.on('update-downloaded', (info) => {
-      log.info('Update downloaded; will install in 5 seconds');
-      setTimeout(() => {
-        autoUpdater.quitAndInstall();
-      }, 5000);
-    });
-  });
+		autoUpdater.on("update-downloaded", (info) => {
+			log.info("Auto Updater: Update downloaded; will install in 5 seconds");
+			setTimeout(() => {
+				autoUpdater.quitAndInstall();
+			}, 5000);
+		});
+	});
 
-  nativeTheme.on('updated', () => {
-    if (nativeTheme.shouldUseDarkColors) {
-      mb.window.webContents.send('gitify:update-theme', 'DARK');
-    } else {
-      mb.window.webContents.send('gitify:update-theme', 'LIGHT');
-    }
-  });
+	nativeTheme.on("updated", () => {
+		if (nativeTheme.shouldUseDarkColors) {
+			mb.window.webContents.send("gitify:update-theme", "DARK");
+		} else {
+			mb.window.webContents.send("gitify:update-theme", "LIGHT");
+		}
+	});
 
-  /**
-   * Gitify custom IPC events
-   */
-  ipc.handle('gitify:version', () => app.getVersion());
+	/**
+	 * Gitify custom IPC events
+	 */
+	ipc.handle("gitify:version", () => app.getVersion());
 
-  ipc.on('gitify:window-show', () => mb.showWindow());
+	ipc.on("gitify:window-show", () => mb.showWindow());
 
-  ipc.on('gitify:window-hide', () => mb.hideWindow());
+	ipc.on("gitify:window-hide", () => mb.hideWindow());
 
-  ipc.on('gitify:quit', () => mb.app.quit());
+	ipc.on("gitify:quit", () => mb.app.quit());
 
-  ipc.on('gitify:icon-active', () => {
-    if (!mb.tray.isDestroyed()) {
-      mb.tray.setImage(activeIcon);
-    }
-  });
+	ipc.on("gitify:icon-active", () => {
+		if (!mb.tray.isDestroyed()) {
+			mb.tray.setImage(activeIcon);
+		}
+	});
 
-  ipc.on('gitify:icon-idle', () => {
-    if (!mb.tray.isDestroyed()) {
-      mb.tray.setImage(idleIcon);
-    }
-  });
+	ipc.on("gitify:icon-idle", () => {
+		if (!mb.tray.isDestroyed()) {
+			mb.tray.setImage(idleIcon);
+		}
+	});
 
-  ipc.on('gitify:update-title', (_, title) => {
-    if (!mb.tray.isDestroyed()) {
-      mb.tray.setTitle(title);
-    }
-  });
+	ipc.on("gitify:update-title", (_, title) => {
+		if (!mb.tray.isDestroyed()) {
+			mb.tray.setTitle(title);
+		}
+	});
 
-  ipc.on(
-    'gitify:update-keyboard-shortcut',
-    (_, { enabled, keyboardShortcut }) => {
-      if (!enabled) {
-        globalShortcut.unregister(keyboardShortcut);
-        return;
-      }
+	ipc.on(
+		"gitify:update-keyboard-shortcut",
+		(_, { enabled, keyboardShortcut }) => {
+			if (!enabled) {
+				globalShortcut.unregister(keyboardShortcut);
+				return;
+			}
 
-      globalShortcut.register(keyboardShortcut, () => {
-        if (mb.window.isVisible()) {
-          mb.hideWindow();
-        } else {
-          mb.showWindow();
-        }
-      });
-    },
-  );
+			globalShortcut.register(keyboardShortcut, () => {
+				if (mb.window.isVisible()) {
+					mb.hideWindow();
+				} else {
+					mb.showWindow();
+				}
+			});
+		},
+	);
 
-  ipc.on('gitify:update-auto-launch', (_, settings) => {
-    app.setLoginItemSettings(settings);
-  });
+	ipc.on("gitify:update-auto-launch", (_, settings) => {
+		app.setLoginItemSettings(settings);
+	});
 });
 
 function checkForUpdates() {
-  log.info('Checking for updates...');
-  autoUpdater.checkForUpdatesAndNotify();
+	log.info("Auto Updater: Checking for updates...");
+	autoUpdater.checkForUpdatesAndNotify();
 }
