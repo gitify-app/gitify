a87e38e7783905a6a00d34ec090f622fafc80223
Adam Setch <adam.setch@outlook.com>
2024-08-08 17:07:04 -0400

    fix: offline emoji needs to be sourced locally
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

---
+++ assets/svg/README.md
@@ -0,0 +1,7 @@
+# Twemoji - Offline SVGs
+
+The SVGs in this folder are copied from https://github.com/jdecked/twemoji/tree/gh-pages/svg
+
+We store some of them locally when they're needed for offline access (eg: network connectivity error).
+
+For all others, we fetch from the CDN.
--- src/components/icons/Emoji.tsx
+++ src/components/icons/Emoji.tsx
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import twemoji from '@twemoji/api';
 import type { FC } from 'react';
 
@@ -5,11 +6,32 @@ export interface IEmoji {
   emoji: string;
 }
 
+type TwemojiOptions = {
+  base: string;
+  size: string;
+  ext: string;
+};
+
 export const Emoji: FC<IEmoji> = (props: IEmoji) => {
   // Generate the HTML for the emoji using twemoji
   const emojiHtml = twemoji.parse(props.emoji, {
     folder: 'svg',
     ext: '.svg',
+    callback: (icon: string, options: TwemojiOptions, _variant: string) => {
+      // Offline
+      let source: string;
+
+      switch (icon) {
+        // Offline Emojis sourced from ../assets/svg
+        case '1f6dc': // 🛜 wifi/network
+          source = path.resolve(`${__dirname}/../../assets`);
+          break;
+        default:
+          source = options.base;
+          break;
+      }
+      return ''.concat(source, '/', options.size, '/', icon, options.ext);
+    },
   });
 
   // biome-ignore lint/security/noDangerouslySetInnerHtml: used for cross-platform emoji compatibility
