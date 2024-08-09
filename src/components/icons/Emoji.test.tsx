b06367914d3f5124b69d3afdd7a9ac66c3c0399f
Adam Setch <adam.setch@outlook.com>
2024-08-08 21:33:58 -0400

    increase coverage
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/components/icons/Emoji.test.tsx
+++ src/components/icons/Emoji.test.tsx
@@ -2,9 +2,17 @@ import { render } from '@testing-library/react';
 import { Emoji, type IEmoji } from './Emoji';
 
 describe('components/icons/Emoji.tsx', () => {
-  it('should render', () => {
+  it('should render - online CDN SVGs', () => {
     const props: IEmoji = {
       emoji: '🍺',
+    };
+    const tree = render(<Emoji {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render - offline SVGs', () => {
+    const props: IEmoji = {
+      emoji: '🛜',
     };
     const tree = render(<Emoji {...props} />);
     expect(tree).toMatchSnapshot();
