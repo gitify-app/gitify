0a2cc8edd740e4431b59ff7269d82757aaf2d5a8
Adam Setch <adam.setch@outlook.com>
2024-10-10 15:19:51 -0400

    fix: discussions url lookup
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/utils/helpers.ts
+++ src/renderer/utils/helpers.ts
@@ -123,7 +123,8 @@ export async function generateGitHubWebUrl(
 ): Promise<Link> {
   const url = new URL(notification.repository.html_url);
 
-  // FIXME: upstream API is returning broken API URLs for Discussion types
+  // FIXME upstream GitHub API has started returning subject urls for Discussion notification types,
+  // however these URLs are broken.  Temporarily downgrading to use discussion lookup process.
   if (notification.subject.type === 'Discussion') {
     notification.subject.url = null;
     notification.subject.latest_comment_url = null;
