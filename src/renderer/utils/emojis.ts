f85b04a1ef1cd9f6a68e3cb2d4b948c500f80d51
Adam Setch <adam.setch@outlook.com>
2024-10-12 09:46:01 -0400

    fix: emoji parse for 🏖️
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/utils/emojis.ts
+++ src/renderer/utils/emojis.ts
@@ -30,6 +30,6 @@ export function convertTextToEmojiImgHtml(text: string): string {
 
 function extractSvgFilename(imgHtml: string): string {
   const srcMatch = imgHtml.match(/src="(.*)"/);
-  const filenameMatch = srcMatch[1].match(/\/([a-z0-9\-]+\.svg)/);
+  const filenameMatch = srcMatch[1].match(/([a-z0-9\-]+\.svg$)/);
   return filenameMatch[1];
 }
