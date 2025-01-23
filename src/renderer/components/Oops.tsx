6eae350e5ef83124dc57d76c0282531405fb8017
Adam Setch <adam.setch@outlook.com>
2025-01-23 08:49:01 -0500

    refactor: error page fallback
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/components/Oops.tsx
+++ src/renderer/components/Oops.tsx
@@ -1,6 +1,7 @@
 import { type FC, useMemo } from 'react';
 
 import type { GitifyError } from '../types';
+import { Errors } from '../utils/errors';
 import { EmojiSplash } from './layout/EmojiSplash';
 
 interface IOops {
@@ -9,16 +10,18 @@ interface IOops {
 }
 
 export const Oops: FC<IOops> = ({ error, fullHeight = true }: IOops) => {
+  const err = error ?? Errors.UNKNOWN;
+
   const emoji = useMemo(
-    () => error.emojis[Math.floor(Math.random() * error.emojis.length)],
-    [error],
+    () => err.emojis[Math.floor(Math.random() * err.emojis.length)],
+    [err],
   );
 
   return (
     <EmojiSplash
       emoji={emoji}
-      heading={error.title}
-      subHeadings={error.descriptions}
+      heading={err.title}
+      subHeadings={err.descriptions}
       fullHeight={fullHeight}
     />
   );
