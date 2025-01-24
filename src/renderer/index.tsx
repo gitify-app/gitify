21714c6b399d3526471d92289bb7ce03482ec911
Brendan Mulholland <github@bmulholland.ca>
2025-01-24 12:36:11 +0100

    chore(deps): update tailwindcss to v4 (#1771)
    * chore(deps): update tailwindcss to v4
    
    * consolidate into App.css
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    
    ---------
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    Co-authored-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
 import 'nprogress/nprogress.css';
-import '../../assets/css/main.css';
 import { App } from './App';
 
 const container = document.getElementById('root');
