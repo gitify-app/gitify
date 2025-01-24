77423aeba77fec42f2e57b65923af0e8518f0a8c
Brendan Mulholland <brendan@recitalapp.com>
2025-01-24 11:30:15 +0100

    chore(deps): update tailwindcss to v4

--- src/renderer/components/fields/Tooltip.tsx
+++ src/renderer/components/fields/Tooltip.tsx
@@ -19,7 +19,7 @@ export const Tooltip: FC<ITooltip> = (props: ITooltip) => {
     >
       <QuestionIcon className="ml-1 text-gitify-tooltip-icon" />
       {showTooltip && (
-        <div className="absolute left-[-80px] z-10 w-60 rounded border border-gray-300 p-2 shadow bg-gitify-tooltip-popout">
+        <div className="absolute left-[-80px] z-10 w-60 rounded-sm border border-gray-300 p-2 shadow-sm bg-gitify-tooltip-popout">
           <div className="text-left text-xs text-gitify-font">
             {props.tooltip}
           </div>
