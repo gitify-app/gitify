26f583bf83f2c1a0579f4e9bf9aa4b1274aa4283
Adam Setch <adam.setch@outlook.com>
2025-02-11 13:11:15 -0600

    refactor: context isolation via preload script
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,11 +1,18 @@
-import { contextBridge, ipcRenderer } from 'electron';
+import { contextBridge, ipcRenderer, shell } from 'electron';
 import { namespacedEvent } from '../shared/events';
 
+import { type Link, OpenPreference } from '../renderer/types';
 import { Constants } from '../renderer/utils/constants';
+import { isLinux, isMacOS, isWindows } from '../shared/platform';
 
 const api = {
-  openExternalLink: (url) =>
-    ipcRenderer.send(namespacedEvent('open-external-link'), url),
+  openExternalLink: (url: Link, openPreference: OpenPreference) => {
+    console.log('PRELOAD OPEN LINK');
+
+    shell.openExternal(url, {
+      activate: openPreference === OpenPreference.FOREGROUND,
+    });
+  },
 
   getAppVersion: () => {
     if (process.env.NODE_ENV === 'development') {
@@ -15,34 +22,28 @@ const api = {
     ipcRenderer.invoke(namespacedEvent('version'));
   },
 
-  encryptValue: (value) =>
+  encryptValue: (value: string) =>
     ipcRenderer.invoke(namespacedEvent('safe-storage-encrypt'), value),
 
-  decryptValue: (value) =>
+  decryptValue: (value: string) =>
     ipcRenderer.invoke(namespacedEvent('safe-storage-decrypt'), value),
 
-  quitApp: () => {
-    console.log('PRELOAD DEBUGGING - QUIT APP');
-
-    ipcRenderer.send(namespacedEvent('quit'));
-  },
+  quitApp: () => ipcRenderer.send(namespacedEvent('quit')),
 
   showWindow: () => ipcRenderer.send(namespacedEvent('window-show')),
 
   hideWindow: () => ipcRenderer.send(namespacedEvent('window-hide')),
 
-  setAutoLaunch: (value) =>
+  setAutoLaunch: (value: boolean) =>
     ipcRenderer.send(namespacedEvent('update-auto-launch'), {
       openAtLogin: value,
       openAsHidden: value,
     }),
 
-  setAlternateIdleIcon: (value) =>
+  setAlternateIdleIcon: (value: boolean) =>
     ipcRenderer.send(namespacedEvent('use-alternate-idle-icon'), value),
 
-  setKeyboardShortcut: (keyboardShortcut) => {
-    console.log('PRELOAD DEBUGGING - setKeyboardShortcut');
-
+  setKeyboardShortcut: (keyboardShortcut: boolean) => {
     ipcRenderer.send(namespacedEvent('update-keyboard-shortcut'), {
       enabled: keyboardShortcut,
       keyboardShortcut: Constants.DEFAULT_KEYBOARD_SHORTCUT,
@@ -50,8 +51,6 @@ const api = {
   },
 
   updateTrayIcon: (notificationsLength = 0) => {
-    console.log('PRELOAD DEBUGGING - UPDATE TRAY ICON');
-
     if (notificationsLength < 0) {
       ipcRenderer.send(namespacedEvent('icon-error'));
       return;
@@ -63,22 +62,21 @@ const api = {
     }
 
     ipcRenderer.send(namespacedEvent('icon-idle'));
-    // ipcRenderer.send(namespacedEvent('update-tray-icon'), notificationsLength),
   },
 
   updateTrayTitle: (title = '') =>
     ipcRenderer.send(namespacedEvent('update-title'), title),
 
   isLinux: () => {
-    return process.platform === 'linux';
+    return isLinux();
   },
 
   isMacOS: () => {
-    return process.platform === 'darwin';
+    return isMacOS();
   },
 
   isWindows: () => {
-    return process.platform === 'win32';
+    return isWindows();
   },
 };
 
--- src/renderer/utils/comms.ts
+++ src/renderer/utils/comms.ts
@@ -1,97 +1,60 @@
-// import { shell } from 'electron';
-// import { namespacedEvent } from '../../shared/events';
-// import { defaultSettings } from '../context/App';
+import { defaultSettings } from '../context/App';
 import type { Link } from '../types';
-// import { Constants } from './constants';
-// import { loadState } from './storage';
+import { loadState } from './storage';
 
-export function openExternalLink(_url: Link): void {
-  // if (url.toLowerCase().startsWith('https://')) {
-  //   // Load the state from local storage to avoid having to pass settings as a parameter
-  //   const { settings } = loadState();
-  //   const_openPreference = settings
-  //     ? settings.openLinks
-  //     : defaultSettings.openLinks;
-  //   // shell.openExternal(url, {
-  //   //   activate: openPreference === OpenPreference.FOREGROUND,
-  //   // });
-  // }
+export function openExternalLink(url: Link): void {
+  // Load the state from local storage to avoid having to pass settings as a parameter
+  const { settings } = loadState();
+  const openPreference = settings
+    ? settings.openLinks
+    : defaultSettings.openLinks;
+
+  if (url.toLowerCase().startsWith('https://')) {
+    console.log('COMMS OPEN EXTERNAL LINK');
+    window.gitify.openExternalLink(url, openPreference);
+  }
 }
 
-export async function getAppVersion(): Promise<string> {
-  return await window.gitify.getAppVersion();
-  // return await ipcRenderer.invoke(namespacedEvent('version'));
+export function getAppVersion(): string {
+  return window.gitify.getAppVersion();
 }
 
 export async function encryptValue(value: string): Promise<string> {
   return await window.gitify.encryptValue(value);
-
-  // return await ipcRenderer.invoke(
-  //   namespacedEvent('safe-storage-encrypt'),
-  //   value,
-  // );
 }
 
 export async function decryptValue(value: string): Promise<string> {
   return await window.gitify.decryptValue(value);
-  // return await ipcRenderer.invoke(
-  //   namespacedEvent('safe-storage-decrypt'),
-  //   value,
-  // );
 }
 
 export function quitApp(): void {
   window.gitify.quitApp();
-  // ipcRenderer.send(namespacedEvent('quit'));
 }
 
 export function showWindow(): void {
   window.gitify.showWindow();
-  // ipcRenderer.send(namespacedEvent('window-show'));
 }
 
 export function hideWindow(): void {
   window.gitify.hideWindow();
-  // ipcRenderer.send(namespacedEvent('window-hide'));
 }
 
 export function setAutoLaunch(value: boolean): void {
   window.gitify.setAutoLaunch(value);
-  // ipcRenderer.send(namespacedEvent('update-auto-launch'), {
-  //   openAtLogin: value,
-  //   openAsHidden: value,
-  // });
 }
 
 export function setAlternateIdleIcon(value: boolean): void {
   window.gitify.setAlternateIdleIcon(value);
-  // ipcRenderer.send(namespacedEvent('use-alternate-idle-icon'), value);
 }
 
 export function setKeyboardShortcut(keyboardShortcut: boolean): void {
   window.gitify.setKeyboardShortcut(keyboardShortcut);
-  // ipcRenderer.send(namespacedEvent('update-keyboard-shortcut'), {
-  //   enabled: keyboardShortcut,
-  //   keyboardShortcut: Constants.DEFAULT_KEYBOARD_SHORTCUT,
-  // });
 }
 
 export function updateTrayIcon(notificationsLength = 0): void {
   window.gitify.updateTrayIcon(notificationsLength);
-  //   if (notificationsLength < 0) {
-  //     ipcRenderer.send(namespacedEvent('icon-error'));
-  //     return;
-  //   }
-
-  //   if (notificationsLength > 0) {
-  //     ipcRenderer.send(namespacedEvent('icon-active'));
-  //     return;
-  //   }
-
-  //   ipcRenderer.send(namespacedEvent('icon-idle'));
 }
 
 export function updateTrayTitle(title = ''): void {
   window.gitify.updateTrayTitle(title);
-  // ipcRenderer.send(namespacedEvent('update-title'), title);
 }
