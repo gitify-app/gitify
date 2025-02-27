b0bc0db707fb96818c7a877fd6762c085db5f170
Adam Setch <adam.setch@outlook.com>
2025-02-26 23:54:33 -0500

    refactor: layout components
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/components/primitives/Footer.tsx
+++ src/renderer/components/primitives/Footer.tsx
@@ -1,16 +1,16 @@
 import { Box, Stack } from '@primer/react';
 import type { FC, ReactNode } from 'react';
 
-interface FooterProps {
+interface IFooter {
   children: ReactNode;
-  justify: 'end' | 'space-between' | 'start' | 'center';
+  justify: 'end' | 'space-between';
 }
 
 /**
  * Footer component displays actions at the bottom of the page.
  * It is fixed to the viewport bottom.
  */
-export const Footer: FC<FooterProps> = ({ children, justify }) => {
+export const Footer: FC<IFooter> = ({ children, justify }) => {
   return (
     <Box className="left-sidebar bg-gitify-footer">
       <Stack direction="horizontal" justify={justify} padding="condensed">
