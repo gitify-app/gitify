38c59e6265d6e18c473bdb15c63a02755ae0620b
Adam Setch <adam.setch@outlook.com>
2025-04-03 08:02:28 -0400

    fix: notarize process
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- scripts/afterSign.js
+++ scripts/afterSign.js
@@ -1,9 +1,6 @@
 const { notarize } = require('@electron/notarize');
 const { AfterPackContext } = require('electron-builder');
 
-const builderConfig = require('../config/electron-builder');
-const appBundleId = builderConfig.appId;
-
 function logAfterSignProgress(msg) {
   // biome-ignore lint/suspicious/noConsoleLog: log notarizing progress
   console.log(`  • [afterSign]: ${msg}`);
@@ -26,26 +23,11 @@ const afterSign = async (context) => {
     return;
   }
 
-  notarizeApp(appName, appOutDir);
-
-  logAfterSignProgress('Completed');
-};
-
-/**
- * Notarizes the application
- * @param {string} appOutDir
- * @param {string} appName
- */
-const notarizeApp = (appOutDir, appName) => {
-  logAfterSignProgress('notarizing app');
-
-  return notarize({
-    appBundleId,
+  return await notarize({
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID_USERNAME,
     appleIdPassword: process.env.APPLE_ID_PASSWORD,
     teamId: process.env.APPLE_ID_TEAM_ID,
-    tool: 'notarytool',
   });
 };
 
