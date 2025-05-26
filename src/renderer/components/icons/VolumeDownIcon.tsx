6f94f7ef6d72d705f57ab07b5bbf2ac8d8f6034b
Yuriy Matskanyuk <yuriy.matskanyuk@gmail.com>
2025-05-26 17:14:53 +0500

    refactor: volume up and down icons (#2023)
    * refactor: volume up and down icons
    
    * fix snapshot
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    
    ---------
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>
    Co-authored-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/components/icons/VolumeDownIcon.tsx
+++ src/renderer/components/icons/VolumeDownIcon.tsx
@@ -9,6 +9,7 @@ export const VolumeDownIcon: FC = () => (
     height="24"
     role="img"
     aria-label="Volume Down Icon"
+    fill="currentColor"
   >
     <g transform="translate(2, 0)">
       <path d="M11.553 3.064A.75.75 0 0 1 12 3.75v16.5a.75.75 0 0 1-1.255.555L5.46 16H2.75A1.75 1.75 0 0 1 1 14.25v-4.5C1 8.784 1.784 8 2.75 8h2.71l5.285-4.805a.752.752 0 0 1 .808-.13ZM10.5 5.445l-4.245 3.86a.748.748 0 0 1-.505.195h-3a.25.25 0 0 0-.25.25v4.5c0 .138.112.25.25.25h3c.187 0 .367.069.505.195l4.245 3.86z" />
