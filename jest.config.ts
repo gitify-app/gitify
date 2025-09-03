c0cbc5be92cb9e2dc6c8d1204014a648f03f3b8f
Adam Setch <adam.setch@outlook.com>
2025-09-03 07:12:26 -0400

    collect coverage from
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
   setupFilesAfterEnv: ['<rootDir>/src/renderer/__helpers__/jest.setup.ts'],
   testEnvironment: 'jsdom',
   collectCoverage: true,
+  collectCoverageFrom: ['src/**/*', '!**/__snapshots__/**'],
   moduleNameMapper: {
     // Force CommonJS build for http adapter to be available.
     // via https://github.com/axios/axios/issues/5101#issuecomment-1276572468
