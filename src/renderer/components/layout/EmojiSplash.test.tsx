aee2b0f0c7b1eef30b54f89e68caa4d6b283d8e3
Adam Setch <adam.setch@outlook.com>
2025-09-03 07:04:18 -0400

    Update src/renderer/components/layout/EmojiSplash.test.tsx
    Co-authored-by: Afonso Jorge Ramos <afonsojorgeramos@gmail.com>

--- src/renderer/components/layout/EmojiSplash.test.tsx
+++ src/renderer/components/layout/EmojiSplash.test.tsx
@@ -1,5 +1,4 @@
-import { render } from '@testing-library/react';
-import { act } from 'react';
+import { act, render } from '@testing-library/react';
 
 import { EmojiSplash } from './EmojiSplash';
 
