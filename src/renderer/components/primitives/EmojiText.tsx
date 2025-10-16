0a797550db1abf0a613c1d9fef8b520ab35ddb6c
Adam Setch <adam.setch@outlook.com>
2025-10-16 15:41:12 -0700

    fix: error emoji text component (#2307)
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/components/primitives/EmojiText.tsx
+++ src/renderer/components/primitives/EmojiText.tsx
@@ -1,22 +1,33 @@
 import { type FC, useEffect, useRef } from 'react';
 
 import { convertTextToEmojiImgHtml } from '../../utils/emojis';
-
 export interface IEmojiText {
   text: string;
 }
-
 export const EmojiText: FC<IEmojiText> = ({ text }) => {
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement | null>(null);
+  const mountedRef = useRef(true);
 
   useEffect(() => {
+    mountedRef.current = true;
+
     const updateEmojiText = async () => {
+      const emojiHtml = await convertTextToEmojiImgHtml(text);
+
+      if (!mountedRef.current) {
+        return;
+      }
+
       if (ref.current) {
-        const emojiHtml = await convertTextToEmojiImgHtml(text);
         ref.current.innerHTML = emojiHtml;
       }
     };
+
     updateEmojiText();
+
+    return () => {
+      mountedRef.current = false;
+    };
   }, [text]);
 
   return <div className="text-7xl" ref={ref} />;
