26b277c011d7d489eaa4c1776dfdd6066d2e6e41
Adam Setch <adam.setch@outlook.com>
2025-11-08 16:46:25 -0500

    Merge branch 'main' into feat/count-unread-only
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/utils/notifications/native.test.ts
+++ src/renderer/utils/notifications/native.test.ts
@@ -1,135 +1,54 @@
-// import { waitFor } from '@testing-library/react';
+import { waitFor } from '@testing-library/react';
 
-// import {
-//   mockAccountNotifications,
-//   mockSingleAccountNotifications,
-// } from '../../__mocks__/notifications-mocks';
-// import { mockAuth } from '../../__mocks__/state-mocks';
-// import { defaultSettings } from '../../context/defaults';
-// import type { SettingsState } from '../../types';
-// import * as native from './native';
+import {
+  mockAccountNotifications,
+  mockSingleAccountNotifications,
+} from '../../__mocks__/notifications-mocks';
+import * as native from './native';
 
-// const raiseSoundNotificationMock = jest.spyOn(native, 'raiseSoundNotification');
+describe('renderer/utils/notifications/native.ts', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-// describe('renderer/utils/notifications/native.ts', () => {
-//   afterEach(() => {
-//     jest.clearAllMocks();
-//   });
+  describe('triggerNativeNotifications', () => {
+    it('should raise a native notification for a single new notification', async () => {
+      native.raiseNativeNotification(
+        mockSingleAccountNotifications[0].notifications,
+      );
 
-//   describe('triggerNativeNotifications', () => {
-//     it('should raise a native notification and play sound for a single new notification', async () => {
-//       const settings: SettingsState = {
-//         ...defaultSettings,
-//         playSound: true,
-//         showNotifications: true,
-//       };
+      // wait for async native handling (generateGitHubWebUrl) to complete
+      await waitFor(() =>
+        expect(window.gitify.raiseNativeNotification).toHaveBeenCalledTimes(1),
+      );
 
-//       native.triggerNativeNotifications([], mockSingleAccountNotifications, {
-//         auth: mockAuth,
-//         settings,
-//       });
+      expect(window.gitify.raiseNativeNotification).toHaveBeenCalledWith(
+        expect.stringContaining(
+          mockSingleAccountNotifications[0].notifications[0].repository
+            .full_name,
+        ),
+        expect.stringContaining(
+          mockSingleAccountNotifications[0].notifications[0].subject.title,
+        ),
+        expect.stringContaining(
+          mockSingleAccountNotifications[0].notifications[0].repository
+            .html_url,
+        ),
+      );
+    });
 
-//       // wait for async native handling (generateGitHubWebUrl) to complete
-//       await waitFor(() =>
-//         expect(window.gitify.raiseNativeNotification).toHaveBeenCalledTimes(1),
-//       );
+    it('should raise a native notification for multiple new notifications', async () => {
+      native.raiseNativeNotification(mockAccountNotifications[0].notifications);
 
-//       expect(window.gitify.raiseNativeNotification).toHaveBeenCalledWith(
-//         expect.stringContaining(
-//           mockSingleAccountNotifications[0].notifications[0].repository
-//             .full_name,
-//         ),
-//         expect.stringContaining(
-//           mockSingleAccountNotifications[0].notifications[0].subject.title,
-//         ),
-//         expect.stringContaining(
-//           mockSingleAccountNotifications[0].notifications[0].repository
-//             .html_url,
-//         ),
-//       );
+      await waitFor(() =>
+        expect(window.gitify.raiseNativeNotification).toHaveBeenCalledTimes(1),
+      );
 
-//       await waitFor(() =>
-//         expect(raiseSoundNotificationMock).toHaveBeenCalledTimes(1),
-//       );
-//       expect(raiseSoundNotificationMock).toHaveBeenCalledWith(0.2);
-//     });
-
-//     it('should raise a native notification and play sound for multiple new notifications', async () => {
-//       const settings: SettingsState = {
-//         ...defaultSettings,
-//         playSound: true,
-//         showNotifications: true,
-//       };
-
-//       native.triggerNativeNotifications([], mockAccountNotifications, {
-//         auth: mockAuth,
-//         settings,
-//       });
-
-//       await waitFor(() =>
-//         expect(window.gitify.raiseNativeNotification).toHaveBeenCalledTimes(1),
-//       );
-
-//       expect(window.gitify.raiseNativeNotification).toHaveBeenCalledWith(
-//         'Gitify',
-//         'You have 4 notifications',
-//         null,
-//       );
-
-//       await waitFor(() =>
-//         expect(raiseSoundNotificationMock).toHaveBeenCalledTimes(1),
-//       );
-//       expect(raiseSoundNotificationMock).toHaveBeenCalledWith(0.2);
-//     });
-
-//     it('should not raise a native notification or play a sound when there are no new notifications', () => {
-//       const settings: SettingsState = {
-//         ...defaultSettings,
-//         playSound: true,
-//         showNotifications: true,
-//       };
-
-//       native.triggerNativeNotifications(
-//         mockSingleAccountNotifications,
-//         mockSingleAccountNotifications,
-//         {
-//           auth: mockAuth,
-//           settings,
-//         },
-//       );
-
-//       expect(window.gitify.raiseNativeNotification).not.toHaveBeenCalled();
-//       expect(raiseSoundNotificationMock).not.toHaveBeenCalled();
-//     });
-
-//     it('should not raise a native notification or play a sound when there are zero notifications', () => {
-//       const settings: SettingsState = {
-//         ...defaultSettings,
-//         playSound: true,
-//         showNotifications: true,
-//       };
-
-//       native.triggerNativeNotifications([], [], {
-//         auth: mockAuth,
-//         settings,
-//       });
-
-//       expect(window.gitify.raiseNativeNotification).not.toHaveBeenCalled();
-//       expect(raiseSoundNotificationMock).not.toHaveBeenCalled();
-//     });
-
-//     it('should not raise a native notification when setting disabled', () => {
-//       const settings: SettingsState = {
-//         ...defaultSettings,
-//         showNotifications: false,
-//       };
-
-//       native.triggerNativeNotifications([], mockAccountNotifications, {
-//         auth: mockAuth,
-//         settings,
-//       });
-
-//       expect(window.gitify.raiseNativeNotification).not.toHaveBeenCalled();
-//     });
-//   });
-// });
+      expect(window.gitify.raiseNativeNotification).toHaveBeenCalledWith(
+        'Gitify',
+        'You have 2 notifications',
+        null,
+      );
+    });
+  });
+});
