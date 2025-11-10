1ab715e18d148308e99bf9a7eb4c037306361e28
Adam Setch <adam.setch@outlook.com>
2025-11-10 07:52:44 -0500

    refactor: remote fixme - permanent fix now
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/utils/helpers.ts
+++ src/renderer/utils/helpers.ts
@@ -106,9 +106,11 @@ export async function generateGitHubWebUrl(
 ): Promise<Link> {
   const url = new URL(notification.repository.html_url);
 
-  // FIXME see #1583
-  // Upstream GitHub API has started returning subject urls for Discussion notification types,
-  // however these URLs are broken.  Temporarily downgrading to use discussion lookup process.
+  /**
+   * Discussions - override generic response values for subject and comment urls,
+   * as we will fetch more specific html urls ourselves below
+   * See issue #1583:
+   */
   if (notification.subject.type === 'Discussion') {
     notification.subject.url = null;
     notification.subject.latest_comment_url = null;
