d959814b4b692a04c79cc86b9e6b2c7d614656c6
Adam Setch <adam.setch@outlook.com>
2025-11-19 09:46:36 -0500

    chore: remove stray comment
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/context/App.test.tsx
+++ src/renderer/context/App.test.tsx
@@ -55,8 +55,6 @@ describe('renderer/context/App.tsx', () => {
   const markNotificationsAsDoneMock = jest.fn();
   const unsubscribeNotificationMock = jest.fn();
 
-  // Removed unused zoomTimeout
-
   const saveStateSpy = jest
     .spyOn(storage, 'saveState')
     .mockImplementation(jest.fn());
