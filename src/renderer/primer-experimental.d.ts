ff955f93d66da828d7111c06204dfb0277bb228d
Adam Setch <adam.setch@outlook.com>
2025-11-19 08:58:56 -0500

    refactor: remove unused primer experimental
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/primer-experimental.d.ts
+++
@@ -1,6 +0,0 @@
-// Shim module for @primer/react/experimental under commonjs moduleResolution
-// to keep existing import statements working after upgrade to v37.
-// TODO - remove this file after all experimental components have been migrated
-declare module '@primer/react/experimental' {
-  export { Banner } from '@primer/react/lib-esm/Banner';
-}
