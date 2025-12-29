245b9b73799a82f86d4d1a5f6ce626281b64fc06
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2025-12-29 00:26:40 +0000

    refactor: use React useId() hook for filter component IDs

--- src/renderer/components/filters/SubjectTypeFilter.tsx
+++ src/renderer/components/filters/SubjectTypeFilter.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import { type FC, useId } from 'react';
 
 import { BellIcon } from '@primer/octicons-react';
 import { Text } from '@primer/react';
@@ -7,12 +7,14 @@ import { subjectTypeFilter } from '../../utils/notifications/filters';
 import { FilterSection } from './FilterSection';
 
 export const SubjectTypeFilter: FC = () => {
+  const id = useId();
+
   return (
     <FilterSection
       filter={subjectTypeFilter}
       filterSetting="filterSubjectTypes"
       icon={BellIcon}
-      id="filter-subject-type"
+      id={id}
       title="Type"
       tooltip={<Text>Filter notifications by type.</Text>}
     />
