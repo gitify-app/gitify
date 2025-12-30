5c01b5df5c4a36097682220b53ad686966dd5077
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2025-12-30 04:21:55 +0100

    feat: add 'Show read notifications' setting (#708)

--- src/renderer/types.ts
+++ src/renderer/types.ts
@@ -109,6 +109,7 @@ export interface NotificationSettingsState {
   showPills: boolean;
   showNumber: boolean;
   participating: boolean;
+  showReadNotifications: boolean;
   markAsDoneOnOpen: boolean;
   markAsDoneOnUnsubscribe: boolean;
   delayNotificationState: boolean;
