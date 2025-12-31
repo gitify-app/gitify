feef5cf89bc66527104f5f8b55b8e0fbb9473af3
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2025-12-31 03:12:38 +0000

    fix: apply high contrast colors when system theme is selected (#2490)

--- src/renderer/utils/theme.ts
+++ src/renderer/utils/theme.ts
@@ -4,6 +4,8 @@ import { Theme } from '../types';
 
 export const DEFAULT_DAY_COLOR_SCHEME = 'light';
 export const DEFAULT_NIGHT_COLOR_SCHEME = 'dark';
+export const DEFAULT_DAY_HIGH_CONTRAST_COLOR_SCHEME = 'light_high_contrast';
+export const DEFAULT_NIGHT_HIGH_CONTRAST_COLOR_SCHEME = 'dark_high_contrast';
 
 export function mapThemeModeToColorMode(themeMode: Theme): ColorModeWithAuto {
   switch (themeMode) {
