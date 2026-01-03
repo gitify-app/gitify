826cf20e34f188a4cade68b4db9187fb56146203
Adam Setch <adam.setch@outlook.com>
2026-01-04 04:29:57 +1000

    refactor: use default handler defaultURL (#2493)
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/utils/notifications/handlers/repositoryDependabotAlertsThread.ts
+++ src/renderer/utils/notifications/handlers/repositoryDependabotAlertsThread.ts
@@ -4,7 +4,7 @@ import type { OcticonProps } from '@primer/octicons-react';
 import { AlertIcon } from '@primer/octicons-react';
 
 import type { GitifyNotification, GitifySubject, Link } from '../../../types';
-import { DefaultHandler } from './default';
+import { DefaultHandler, defaultHandler } from './default';
 
 class RepositoryDependabotAlertsThreadHandler extends DefaultHandler {
   readonly type = 'RepositoryDependabotAlertsThread';
@@ -14,7 +14,7 @@ class RepositoryDependabotAlertsThreadHandler extends DefaultHandler {
   }
 
   defaultUrl(notification: GitifyNotification): Link {
-    const url = new URL(notification.repository.htmlUrl);
+    const url = new URL(defaultHandler.defaultUrl(notification));
     url.pathname += '/security/dependabot';
     return url.href as Link;
   }
--- src/renderer/utils/notifications/handlers/repositoryInvitation.ts
+++ src/renderer/utils/notifications/handlers/repositoryInvitation.ts
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 import { MailIcon, type OcticonProps } from '@primer/octicons-react';
 
 import type { GitifyNotification, GitifySubject, Link } from '../../../types';
-import { DefaultHandler } from './default';
+import { DefaultHandler, defaultHandler } from './default';
 
 class RepositoryInvitationHandler extends DefaultHandler {
   readonly type = 'RepositoryInvitation';
@@ -13,7 +13,7 @@ class RepositoryInvitationHandler extends DefaultHandler {
   }
 
   defaultUrl(notification: GitifyNotification): Link {
-    const url = new URL(notification.repository.htmlUrl);
+    const url = new URL(defaultHandler.defaultUrl(notification));
     url.pathname += '/invitations';
     return url.href as Link;
   }
