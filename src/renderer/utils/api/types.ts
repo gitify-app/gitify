967ce14582d22fcbe9ef86f5fc6e028817b412d6
Adam Setch <adam.setch@outlook.com>
2026-01-20 16:27:49 -0500

    Merge branch 'main' into refactor/octokit-types-continued
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/utils/api/types.ts
+++ src/renderer/utils/api/types.ts
@@ -8,22 +8,28 @@ export interface GitHubRESTError {
 }
 
 export type ListNotificationsForAuthenticatedUserResponse =
-  Endpoints['GET /notifications']['response'];
+  Endpoints['GET /notifications']['response']['data'];
 
 export type MarkNotificationThreadAsReadResponse =
-  Endpoints['PATCH /notifications/threads/{thread_id}']['response'];
+  Endpoints['PATCH /notifications/threads/{thread_id}']['response']['data'];
 
 export type MarkNotificationThreadAsDoneResponse =
-  Endpoints['DELETE /notifications/threads/{thread_id}']['response'];
+  Endpoints['DELETE /notifications/threads/{thread_id}']['response']['data'];
 
 export type IgnoreNotificationThreadSubscriptionResponse =
-  Endpoints['PUT /notifications/threads/{thread_id}/subscription']['response'];
+  Endpoints['PUT /notifications/threads/{thread_id}/subscription']['response']['data'];
 
 export type GetCommitResponse =
-  Endpoints['GET /repos/{owner}/{repo}/commits/{ref}']['response'];
+  Endpoints['GET /repos/{owner}/{repo}/commits/{ref}']['response']['data'];
 
 export type GetCommitCommentResponse =
-  Endpoints['GET /repos/{owner}/{repo}/comments/{comment_id}']['response'];
+  Endpoints['GET /repos/{owner}/{repo}/comments/{comment_id}']['response']['data'];
 
 export type GetReleaseResponse =
-  Endpoints['GET /repos/{owner}/{repo}/releases/{release_id}']['response'];
+  Endpoints['GET /repos/{owner}/{repo}/releases/{release_id}']['response']['data'];
+
+export type RawGitHubNotification =
+  Endpoints['GET /notifications']['response']['data'][number];
+
+// FIXME - replace this if possible
+export type RawUser = Endpoints['GET /users/{username}']['response']['data'];
