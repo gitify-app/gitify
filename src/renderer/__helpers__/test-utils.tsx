4abbf62102d470c3f0a4e0e9a68bd491ba111757
Adam Setch <adam.setch@outlook.com>
2026-01-25 09:17:41 -0500

    feat: device code flow
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/__helpers__/test-utils.tsx
+++ src/renderer/__helpers__/test-utils.tsx
@@ -38,9 +38,9 @@ function AppContextProvider({ children, value = {} }: AppContextProviderProps) {
       globalError: null,
 
       // Default mock implementations for all required methods
-      startGitHubDeviceFlow: jest.fn(),
-      pollGitHubDeviceFlow: jest.fn(),
-      completeGitHubDeviceLogin: jest.fn(),
+      loginWithDeviceFlowStart: jest.fn(),
+      loginWithDeviceFlowPoll: jest.fn(),
+      loginWithDeviceFlowComplete: jest.fn(),
       loginWithOAuthApp: jest.fn(),
       loginWithPersonalAccessToken: jest.fn(),
       logoutFromAccount: jest.fn(),
