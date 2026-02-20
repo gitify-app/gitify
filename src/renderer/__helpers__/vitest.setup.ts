276536e3fe162b9ec9b4d3029322a466afc35006
Adam Setch <adam.setch@outlook.com>
2026-02-20 12:22:33 -0500

    refactor(filters): use zustand for filters store
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/__helpers__/vitest.setup.ts
+++ src/renderer/__helpers__/vitest.setup.ts
@@ -5,9 +5,6 @@ import { useFiltersStore } from '../stores';
 // Sets timezone to UTC for consistent date/time in tests and snapshots
 process.env.TZ = 'UTC';
 
-// Mock OAuth client ID and secret
-process.env.OAUTH_CLIENT_ID = 'FAKE_CLIENT_ID_123';
-
 /**
  * Reset stores
  */
