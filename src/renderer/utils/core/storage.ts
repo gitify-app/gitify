6da8462d034d76966ca1e08844ad8cd70067bfc5
Adam Setch <adam.setch@outlook.com>
2026-03-24 09:40:15 -0400

    fix: storage keys
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/renderer/utils/core/storage.ts
+++ src/renderer/utils/core/storage.ts
@@ -3,7 +3,7 @@ import { Constants } from '../../constants';
 import type { GitifyState } from '../../types';
 
 export function loadState(): GitifyState {
-  const existing = localStorage.getItem(Constants.STORAGE_KEY);
+  const existing = localStorage.getItem(Constants.STORAGE.LEGACY);
   const { auth, settings } = (existing && JSON.parse(existing)) || {};
   return { auth, settings };
 }
@@ -12,7 +12,7 @@ export function saveState(gitifyState: GitifyState) {
   const auth = gitifyState.auth;
   const settings = gitifyState.settings;
   const settingsString = JSON.stringify({ auth, settings });
-  localStorage.setItem(Constants.STORAGE_KEY, settingsString);
+  localStorage.setItem(Constants.STORAGE.LEGACY, settingsString);
 }
 
 export function clearState() {
